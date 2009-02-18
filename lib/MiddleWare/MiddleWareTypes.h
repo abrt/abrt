65ae557fd0f94b6ffa088fcc4bbcf62a6499934e
Zdenek Prikryl <zprikryl@redhat.com>
2009-02-18 10:06:54 +0100

    added common types headers

---
+++ lib/MiddleWare/MiddleWareTypes.h
@@ -0,0 +1,13 @@
+#ifndef MIDDLEWARETYPES_H_
+#define MIDDLEWARETYPES_H_
+
+#include <map>
+#include <set>
+#include <vector>
+#include <string>
+
+typedef std::vector<std::string> vector_string_t;
+typedef std::map<std::string, std::string> map_string_string_t;
+typedef std::set<std::string> set_strings_t;
+
+#endif /* MIDDLEWARETYPES_H_ */
