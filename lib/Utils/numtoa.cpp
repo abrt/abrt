023123d9752128db9180dacd6adb843d1afe0bc4
Denys Vlasenko <vda.linux@googlemail.com>
2010-02-07 23:09:58 +0100

    forgot to add lib/Utils/numtoa.cpp
    Signed-off-by: Denys Vlasenko <vda.linux@googlemail.com>

---
+++ lib/Utils/numtoa.cpp
@@ -0,0 +1,34 @@
+/*
+    Number to string conversions
+
+    Copyright (C) 2010  ABRT team
+    Copyright (C) 2010  RedHat inc.
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+#include "abrtlib.h"
+
+std::string unsigned_to_string(unsigned long long x)
+{
+	char buf[sizeof(x)*3];
+	sprintf(buf, "%llu", x);
+	return buf;
+}
+std::string signed_to_string(long long x)
+{
+	char buf[sizeof(x)*3];
+	sprintf(buf, "%lld", x);
+	return buf;
+}
