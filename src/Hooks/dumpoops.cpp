927dcf40df250d17bb9c7b2b95fe1255c85636bd
Nikola Pajkovsky <npajkovs@redhat.com>
2010-02-16 12:18:16 +0100

    license fix
    Signed-off-by: Nikola Pajkovsky <npajkovs@redhat.com>

--- src/Hooks/dumpoops.cpp
+++ src/Hooks/dumpoops.cpp
@@ -1,28 +1,25 @@
 /*
- * Copyright 2007, Intel Corporation
- * Copyright 2009, Red Hat Inc.
- *
- * This file is part of Abrt.
- *
- * This program file is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation; version 2 of the License.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program in a file named COPYING; if not, write to the
- * Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor,
- * Boston, MA 02110-1301 USA
- *
- * Authors:
- *      Denys Vlasenko <dvlasenk@redhat.com>
- *      Zdenek Prikryl <zprikryl@redhat.com>
- */
+    Copyright (C) 2010  ABRT team
+    Copyright (C) 2010  RedHat Inc
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+    Authors:
+        Denys Vlasenko <dvlasenk@redhat.com>
+        Zdenek Prikryl <zprikryl@redhat.com>
+*/
 
 #include "abrtlib.h"
 #include "abrt_types.h"
