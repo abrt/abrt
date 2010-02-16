c2dc532ee08aae61fa0164de03b45fe6937881ac
Nikola Pajkovsky <npajkovs@redhat.com>
2010-02-16 12:18:16 +0100

    license fix
    Signed-off-by: Nikola Pajkovsky <npajkovs@redhat.com>

--- lib/Plugins/Bugzilla.cpp
+++ lib/Plugins/Bugzilla.cpp
@@ -16,6 +16,7 @@
     with this program; if not, write to the Free Software Foundation, Inc.,
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
+
 #include <xmlrpc-c/base.h>
 #include <xmlrpc-c/client.h>
 #include "abrtlib.h"
