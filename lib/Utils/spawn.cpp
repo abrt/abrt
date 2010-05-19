5368b089d519764f076164d190e9c0f716e3b27b
Denys Vlasenko <vda.linux@googlemail.com>
2010-05-19 14:12:30 +0200

    trivial fix: fork_execv_on_steroids was closing wrong descriptor
    Signed-off-by: Denys Vlasenko <vda.linux@googlemail.com>

--- lib/Utils/spawn.cpp
+++ lib/Utils/spawn.cpp
@@ -73,7 +73,7 @@ pid_t fork_execv_on_steroids(int flags,
 		}
 		if (flags & EXECFLG_OUTPUT) {
 			xmove_fd(pipe_fm_child[1], STDOUT_FILENO);
-			close(pipe_to_child[0]);
+			close(pipe_fm_child[0]);
 		} else if (flags & EXECFLG_OUTPUT_NUL) {
 			xmove_fd(xopen("/dev/null", O_RDWR), STDOUT_FILENO);
 		}
