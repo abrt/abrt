f03f8111411de6ece4a810fadacd5e019e167cfe
Denys Vlasenko <vda.linux@googlemail.com>
2010-05-25 14:18:23 +0200

    update some utility devel scripts, no impact on users
    Signed-off-by: Denys Vlasenko <vda.linux@googlemail.com>

--- examples/test.sh
+++ examples/test.sh
@@ -1,4 +1,7 @@
 for f in *.test; do
-    b="${f%%.test}"
-    dumpoops -s "$f" >"$b".right 2>&1
+	b="${f%%.test}"
+	dumpoops -s "$f" >"$b".out 2>&1
+	if diff "$b".out "$b".right >"$b".diff 2>&1; then
+		rm "$b".out "$b".diff
+	fi
 done
