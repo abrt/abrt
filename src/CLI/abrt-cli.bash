b779e34fa9b133dad1e183bf8145a017c1bf15d2
Karel Klic <kklic@redhat.com>
2010-06-22 15:08:12 +0200

    Bash completion update

--- src/CLI/abrt-cli.bash
+++ src/CLI/abrt-cli.bash
@@ -1,22 +1,38 @@
 # bash-completion add-on for abrt-cli(1)
 # http://bash-completion.alioth.debian.org/
 
+# $1 = additional options for abrt-cli
+_abrt_list()
+{
+    echo $(abrt-cli --list $1 | grep UUID | awk '{print $3}')
+    return 0
+}
+
 _abrt_cli()
 {
     local cur prev opts
     COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
-    opts="--help --version --get-list --get-list-full --report --report-always --delete"
+    opts="--help --version --list --report --delete"
 
     #
     #  Complete the arguments to some of the basic commands.
     #
     case "${prev}" in
-        --report|--report-always|--delete)
-            local uuids=$(abrt-cli --get-list | grep UUID | awk '{print $3}')
-            COMPREPLY=( $(compgen -W "${uuids}" -- ${cur}) )
-            return 0
+        --list)
+            opts="--full"
+            ;;
+        --report)
+            # Include only not-yet-reported crashes.
+            opts="--always $(_abrt_list)"
+            ;;
+        --always) # This is for --report --always
+            # Include only not-yet-reported crashes.
+            opts=$(_abrt_list)
+            ;;
+        --delete)
+            opts=$(_abrt_list "--full")
             ;;
     esac
 
