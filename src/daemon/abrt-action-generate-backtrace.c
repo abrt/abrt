43c74514d0faa39d343a26f39f31ec7689334b7b
Karel Klic <kklic@redhat.com>
2010-10-14 16:36:12 +0200

    btparser integration: merge it into ABRT's directory hierarchy

--- src/daemon/abrt-action-generate-backtrace.c
+++ src/daemon/abrt-action-generate-backtrace.c
@@ -17,9 +17,9 @@
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 #include "abrtlib.h"
-#include "btparser/lib/backtrace.h"
-#include "btparser/lib/frame.h"
-#include "btparser/lib/location.h"
+#include "../btparser/backtrace.h"
+#include "../btparser/frame.h"
+#include "../btparser/location.h"
 #include "parse_options.h"
 
 
