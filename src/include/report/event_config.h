1292529dafdca6543ce9e85d383effb2c566b79a
Nikola Pajkovsky <npajkovs@redhat.com>
2011-03-07 17:25:15 +0100

    add license and #ifdef EVENT_CONFIG_H around header
    Signed-off-by: Nikola Pajkovsky <npajkovs@redhat.com>

--- src/include/report/event_config.h
+++ src/include/report/event_config.h
@@ -1,4 +1,25 @@
+/*
+    Copyright (C) 2011  ABRT team
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
 #include <glib.h>
+
+#ifndef EVENT_CONFIG_H
+#define EVENT_CONFIG_H
 
 typedef enum
 {
@@ -49,3 +70,5 @@ void free_event_config_data(void);
 event_config_t *get_event_config(const char *name);
 
 extern GList *g_event_config_list;   // for iterating through entire list of all loaded configs
+
+#endif
