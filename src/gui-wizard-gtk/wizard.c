7abba57049799945c731e1ffdc71a2d3e7513999
Denys Vlasenko <dvlasenk@redhat.com>
2011-03-16 10:38:41 +0100

    gui-wizard-gtk: append newline to event log if it has unterminated last line
    Signed-off-by: Denys Vlasenko <dvlasenk@redhat.com>

--- src/gui-wizard-gtk/wizard.c
+++ src/gui-wizard-gtk/wizard.c
@@ -708,14 +708,21 @@ static void save_to_event_log(struct analyze_event_data *evd, const char *str)
 
 static void update_event_log_on_disk(const char *str)
 {
+    /* Load existing log */
     struct dump_dir *dd = dd_opendir(g_dump_dir_name, 0);
     if (!dd)
         return;
     char *event_log = dd_load_text_ext(dd, FILENAME_EVENT_LOG, DD_FAIL_QUIETLY_ENOENT);
 
-    event_log = append_to_malloced_string(event_log, str);
-    char *new_log = event_log;
+    /* Append new log part to existing log */
     unsigned len = strlen(event_log);
+    if (len != 0 && event_log[len - 1] != '\n')
+        event_log = append_to_malloced_string(event_log, "\n");
+    event_log = append_to_malloced_string(event_log, str);
+
+    /* Trim log according to size watermarks */
+    len = strlen(event_log);
+    char *new_log = event_log;
     if (len > EVENT_LOG_HIGH_WATERMARK)
     {
         new_log += len - EVENT_LOG_LOW_WATERMARK;
@@ -724,6 +731,7 @@ static void update_event_log_on_disk(const char *str)
             new_log++;
     }
 
+    /* Save */
     dd_save_text(dd, FILENAME_EVENT_LOG, new_log);
     free(event_log);
     dd_close(dd);
