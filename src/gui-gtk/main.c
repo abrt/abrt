aa08665949da429660dd78a6d26a0fc3cb11d6cd
Jiri Moskovcak <jmoskovc@redhat.com>
2011-03-29 18:10:17 +0200

    gui: fixed the prgname trac#180

--- src/gui-gtk/main.c
+++ src/gui-gtk/main.c
@@ -147,6 +147,11 @@ int main(int argc, char **argv)
     textdomain(PACKAGE);
 #endif
 
+    /* without this the name is set to argv[0] which confuses
+     * desktops which uses the name to find the corresponding .desktop file
+     * trac#180
+     */
+    g_set_prgname("abrt");
     gtk_init(&argc, &argv);
 
     char *env_verbose = getenv("ABRT_VERBOSE");
