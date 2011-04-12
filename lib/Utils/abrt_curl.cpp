035aa63fb33d7c4a21b4400e7b6809744af1fa2e
Jiri Moskovcak <jmoskovc@redhat.com>
2011-04-12 15:58:20 +0200

    added forgotten #include <config.h>

--- lib/Utils/abrt_curl.cpp
+++ lib/Utils/abrt_curl.cpp
@@ -16,6 +16,9 @@
     with this program; if not, write to the Free Software Foundation, Inc.,
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
+#if HAVE_CONFIG_H
+# include <config.h>
+#endif
 #include "abrtlib.h"
 #include "abrt_curl.h"
 #include "comm_layer_inner.h"
