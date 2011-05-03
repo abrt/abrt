27e7d86db7c6c96e8da75e8e8dda6359b9e73c41
Denys Vlasenko <dvlasenk@redhat.com>
2011-05-04 00:15:14 +0200

    add abrt-action-print manpage. Closes #238
    Signed-off-by: Denys Vlasenko <dvlasenk@redhat.com>

--- src/plugins/abrt-action-print.c
+++ src/plugins/abrt-action-print.c
@@ -36,7 +36,7 @@ int main(int argc, char **argv)
 
     /* Can't keep these strings/structs static: _() doesn't support that */
     const char *program_usage_string = _(
-        PROGNAME" [-v] -d DIR [-o FILE] [-a yes/no]\n"
+        PROGNAME" [-v] -d DIR [-o FILE] [-a yes/no] [-r]\n"
         "\n"
         "Prints problem information to standard output or FILE"
     );
