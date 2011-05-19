809f48821a861f49b691d9ca05b5022dddab739f
Denys Vlasenko <dvlasenk@redhat.com>
2011-05-19 13:39:15 +0200

    typo fix
    Signed-off-by: Denys Vlasenko <dvlasenk@redhat.com>

--- src/plugins/abrt-action-bugzilla.c
+++ src/plugins/abrt-action-bugzilla.c
@@ -254,7 +254,7 @@ int main(int argc, char **argv)
         "CONFFILE lines should have 'PARAM = VALUE' format.\n"
         "Recognized string parameters: BugzillaURL, Login, Password.\n"
         "Recognized boolean parameter (VALUE should be 1/0, yes/no): SSLVerify.\n"
-        "Parameters can be overridded via $Bugzilla_PARAM environment variables."
+        "Parameters can be overridden via $Bugzilla_PARAM environment variables."
     );
     enum {
         OPT_v = 1 << 0,
