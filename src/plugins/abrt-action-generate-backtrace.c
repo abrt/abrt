9e10e1e7d0a9e22520a2fb9387db067c90944f8b
Martin Milata <mmilata@redhat.com>
2011-12-07 13:25:17 +0100

    a-a-generate-backtrace: remove unused #includes

--- src/plugins/abrt-action-generate-backtrace.c
+++ src/plugins/abrt-action-generate-backtrace.c
@@ -16,9 +16,6 @@
     with this program; if not, write to the Free Software Foundation, Inc.,
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
-#include <btparser/backtrace.h>
-#include <btparser/frame.h>
-#include <btparser/location.h>
 #include "libabrt.h"
 
 
