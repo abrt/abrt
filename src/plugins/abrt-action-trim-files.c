905c65f7c7b5e339369775156eeded2f3fffdbe5
Denys Vlasenko <dvlasenk@redhat.com>
2011-12-09 17:44:00 +0100

    abrt-action-trim-files: extend it to be able to skip files
    abrt-action-install-debuginfo: use this functionality to not delete
    debuginfos we will later need.
    
    Signed-off-by: Denys Vlasenko <dvlasenk@redhat.com>

--- src/plugins/abrt-action-trim-files.c
+++ src/plugins/abrt-action-trim-files.c
@@ -18,10 +18,17 @@
 */
 #include "libabrt.h"
 
+/* TODO: remember N worst files and their sizes, not just one:
+ * if DIR is deep, scanning it takes *a few seconds*.
+ * If we rescan it after each single file deletion, it can be VERY slow.
+ * (I observed ~20 min long case).
+ */
+
 static double get_dir_size(const char *dirname,
-                           char **worst_file,
-                           double *worst_file_size)
-{
+                char **worst_file,
+                double *worst_file_size,
+                char **preserve_list
+) {
     DIR *dp = opendir(dirname);
     if (!dp)
         return 0;
@@ -29,7 +36,6 @@ static double get_dir_size(const char *dirname,
     /* "now" is used only if caller wants to know worst_file */
     time_t now = worst_file ? time(NULL) : 0;
     struct dirent *dent;
-    struct stat stats;
     double size = 0;
     while ((dent = readdir(dp)) != NULL)
     {
@@ -37,29 +43,43 @@ static double get_dir_size(const char *dirname,
             continue;
 
         char *fullname = concat_path_file(dirname, dent->d_name);
+        struct stat stats;
         if (lstat(fullname, &stats) != 0)
-        {
-            free(fullname);
-            continue;
-        }
+            goto next;
 
         if (S_ISDIR(stats.st_mode))
         {
-            double sz = get_dir_size(fullname, worst_file, worst_file_size);
+            double sz = get_dir_size(fullname, worst_file, worst_file_size, preserve_list);
             size += sz;
         }
         else if (S_ISREG(stats.st_mode))
         {
             double sz = stats.st_size;
+            /* Account for filename and inode storage (approximately).
+             * This also makes even zero-length files to have nonzero cost
+             */
+            sz += strlen(dent->d_name) + sizeof(stats);
             size += sz;
 
             if (worst_file)
             {
+                if (preserve_list)
+                {
+                    char **pp = preserve_list;
+                    while (*pp)
+                    {
+                        //log("'%s' ? '%s'", fullname, *pp);
+                        if (strcmp(fullname, *pp) == 0)
+                            goto next;
+                        pp++;
+                    }
+                }
+
                 /* Calculate "weighted" size and age
                  * w = sz_kbytes * age_mins */
                 sz /= 1024;
                 long age = (now - stats.st_mtime) / 60;
-                if (age > 0)
+                if (age > 1)
                     sz *= age;
 
                 if (sz > *worst_file_size)
@@ -71,6 +91,7 @@ static double get_dir_size(const char *dirname,
                 }
             }
         }
+ next:
         free(fullname);
     }
     closedir(dp);
@@ -111,18 +132,19 @@ static void delete_dirs(gpointer data, gpointer exclude_path)
     trim_problem_dirs(dir, cap_size, exclude_path);
 }
 
-static void delete_files(gpointer data, gpointer user_data_unused)
+static void delete_files(gpointer data, gpointer void_preserve_list)
 {
     double cap_size;
     const char *dir = parse_size_pfx(&cap_size, data);
+    char **preserve_list = void_preserve_list;
 
     unsigned count = 1000;
     while (--count != 0)
     {
         char *worst_file = NULL;
         double worst_file_size = 0;
-        double cur_size = get_dir_size(dir, &worst_file, &worst_file_size);
-        if (cur_size <= cap_size)
+        double cur_size = get_dir_size(dir, &worst_file, &worst_file_size, preserve_list);
+        if (cur_size <= cap_size || !worst_file)
         {
             VERB2 log("cur_size:%.0f cap_size:%.0f, no (more) trimming", cur_size, cap_size);
             free(worst_file);
@@ -146,7 +168,7 @@ int main(int argc, char **argv)
 
     /* Can't keep these strings/structs static: _() doesn't support that */
     const char *program_usage_string = _(
-        "& [-v] [-d SIZE:DIR]... [-f SIZE:DIR]... [-p DIR]\n"
+        "& [-v] [-d SIZE:DIR]... [-f SIZE:DIR]... [-p DIR] [FILE]...\n"
         "\n"
         "Deletes problem dirs (-d) or files (-f) in DIRs until they are smaller than SIZE"
     );
@@ -159,20 +181,24 @@ int main(int argc, char **argv)
     /* Keep enum above and order of options below in sync! */
     struct options program_options[] = {
         OPT__VERBOSE(&g_verbose),
-        OPT_LIST(  'd', NULL, &dir_list , "SIZE:DIR", _("Delete dump dirs")),
-        OPT_LIST(  'f', NULL, &file_list, "SIZE:DIR", _("Delete files")),
-        OPT_STRING('p', NULL, &preserve , "DIR"     , _("Preserve this dump dir")),
+        OPT_LIST('d'  , NULL, &dir_list , "SIZE:DIR", _("Delete whole problem directories")),
+        OPT_LIST('f'  , NULL, &file_list, "SIZE:DIR", _("Delete files inside this directory")),
+        OPT_STRING('p', NULL, &preserve,  "DIR"     , _("Preserve this directory")),
         OPT_END()
     };
     /*unsigned opts =*/ parse_opts(argc, argv, program_options, program_usage_string);
     argv += optind;
-    if (argv[0] || !(dir_list || file_list))
+    if ((argv[0] && !file_list)
+     || !(dir_list || file_list)
+    ) {
         show_usage_and_die(program_usage_string, program_options);
+    }
 
-    export_abrt_envvars(0);
+    /* We don't have children, so this is not needed: */
+    //export_abrt_envvars(/*set_pfx:*/ 0);
 
     g_list_foreach(dir_list, delete_dirs, preserve);
-    g_list_foreach(file_list, delete_files, NULL);
+    g_list_foreach(file_list, delete_files, argv);
 
     return 0;
 }
