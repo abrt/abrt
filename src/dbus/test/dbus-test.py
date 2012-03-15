de4e9eed49eda4d79a61d9ebb589e487937695b1
Jiri <moskovcak@gmail.com>
2012-03-15 21:37:11 +0100

    updated abrt-dbus test

--- src/dbus/test/dbus-test.py
+++ src/dbus/test/dbus-test.py
@@ -3,10 +3,11 @@ import CCDBusBackend
 import sys
 
 dm = CCDBusBackend.DBusManager()
+
 for a in dm.daemon().GetProblems("/var/spool/abrt"):
     print a
-#print dm.daemon().GetInfo("ahoj")
-print ">>> AUTHORIZED <<<"
+print "GetInfo:\n\t", dm.daemon().GetInfo("/var/spool/abrt/pyhook-2012-02-10-15:36:10-19157")
+#print ">>> AUTHORIZED <<<"
 for a in dm.daemon().GetAllProblems("/var/spool/abrt"):
     print a
 #print dm.daemon().Quit()
@@ -14,10 +15,10 @@ for a in dm.daemon().GetAllProblems("/var/spool/abrt"):
 #for a in dm.daemon().GetAllProblems():
 #    print a
 
-try:    
-    dm.daemon().ChownProblemDir("/var/spool/abrt/ccpp-2012-02-08-23:03:29-1113")
-    print "Access allowed, running wizard..."
+#try:    
+#    dm.daemon().ChownProblemDir("/var/spool/abrt/ccpp-2012-02-08-23:03:29-1113")
+#    print "Access allowed, running wizard..."
     # run
     # $ report-gtk -d problem_dir
-except Exception, ex:
-    print "Access not allowed...", str(ex)
+#except Exception, ex:
+#    print "Access not allowed...", str(ex)
