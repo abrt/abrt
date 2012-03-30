fc91ca4433b373e57997d400cc413b4c3de09b7c
Richard Marko <rmarko@redhat.com>
2012-03-30 17:03:55 +0200

    testsuite: point f17 to development, not to snapshot
    Snapshots can't be installed via virt-intall due to rhbz#790348

--- tests/helpers/targets/f17/defs.sh
+++ tests/helpers/targets/f17/defs.sh
@@ -5,9 +5,7 @@ VM_NAME='F17_nightly_run'
 REPO='git://abrt.brq.redhat.com/abrt.git'
 OS_VARIANT='fedora16'
 KS_NAME_PREFIX='fedora_17'
-#LOC='http://download.fedoraproject.org/pub/fedora/linux/development/17/x86_64/os/'
-# temporary snapshot location
-LOC='http://download.englab.brq.redhat.com/pub/fedora/fedora-alt/stage/17-Beta.RC2/Fedora/x86_64/os/'
+LOC='http://download.fedoraproject.org/pub/fedora/linux/development/17/x86_64/os/'
 
 DISK=$( echo /dev/mapper/*f17_vm )
 
