04bfbedab5e0669b0ef4c2ece7d1f849b63c5db0
Jakub Filak <jfilak@redhat.com>
2012-06-27 15:36:32 +0200

    testsuite: add anaconda updates for f16
    Signed-off-by: Jakub Filak <jfilak@redhat.com>

--- tests/helpers/targets/f16/defs.sh
+++ tests/helpers/targets/f16/defs.sh
@@ -8,3 +8,6 @@ KS_NAME_PREFIX='fedora_16'
 LOC='http://download.fedoraproject.org/pub/fedora/linux/releases/16/Fedora/x86_64/os/'
 DISK=$( echo /dev/mapper/*f16_vm )
 
+# http://fedoraproject.org/wiki/Anaconda/Updates
+INIT_EXTRA_ARGS="updates=http://abrt.brq.redhat.com/media/updates-anaconda.img"
+
