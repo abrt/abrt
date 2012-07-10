65fca20ead49397e257e46c16302834d6c4a91fb
Jiri Moskovcak <jmoskovc@redhat.com>
2012-07-10 13:15:47 +0200

    testsuite: our local mirror is fixed, so use it

--- tests/helpers/targets/f16/defs.sh
+++ tests/helpers/targets/f16/defs.sh
@@ -5,7 +5,7 @@ VM_NAME='F16_nightly_run'
 REPO='git://abrt.brq.redhat.com/abrt.git'
 OS_VARIANT='fedora16'
 KS_NAME_PREFIX='fedora_16'
-LOC='http://download.fedoraproject.org/pub/fedora/linux/releases/16/Fedora/x86_64/os/'
+LOC='http://download.englab.brq.redhat.com/pub/fedora/linux/releases/16/Fedora/x86_64/os/'
 DISK=$( echo /dev/mapper/*f16_vm )
 
 # http://fedoraproject.org/wiki/Anaconda/Updates
--- tests/helpers/targets/f17/defs.sh
+++ tests/helpers/targets/f17/defs.sh
@@ -5,7 +5,7 @@ VM_NAME='F17_nightly_run'
 REPO='git://abrt.brq.redhat.com/abrt.git'
 OS_VARIANT='fedora16'
 KS_NAME_PREFIX='fedora_17'
-LOC='http://download.fedoraproject.org/pub/fedora/linux/development/17/x86_64/os/'
+LOC='http://download.englab.brq.redhat.com/pub/fedora/linux/releases/17/Fedora/x86_64/os/'
 
 DISK=$( echo /dev/mapper/*f17_vm )
 
