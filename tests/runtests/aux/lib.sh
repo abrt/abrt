94a330f0b2678106612cf42662c0533a02861149
Richard Marko <rmarko@redhat.com>
2012-11-18 15:55:36 +0100

    testsuite: add wait_for_* library functions

--- tests/runtests/aux/lib.sh
+++ tests/runtests/aux/lib.sh
@@ -17,6 +17,45 @@ function get_crash_path() {
     rlLog "PATH = $crash_PATH"
 }
 
+function wait_for_process() {
+    local procname=$1
+    local timeout=3000
+    if [ $# -gt 1 ]; then
+        timeout=$2
+    fi
+    rlLog "Waiting for $procname to end"
+    local c=0
+    while pidof "$procname" &> /dev/null; do
+        sleep 0.1
+        let c=$c+1
+        if [ $c -gt $timeout ]; then
+            rlFail "Timeout"
+            break
+        fi
+    done
+    t=$( echo "scale=2; $c/10" | bc )
+    rlLog "Process ended in $t seconds"
+}
+
+function wait_for_sosreport() {
+    wait_for_process sosreport
+}
+
+function wait_for_hooks() {
+    rlLog "Waiting for all hooks to end"
+    local c=0
+    while [ ! -f /tmp/abrt-done ]; do
+        sleep 0.1
+        let c=$c+1
+        if [ $c -gt 3000 ]; then
+            rlFail "Timeout"
+            break
+        fi
+    done
+    t=$( echo "scale=2; $c/10" | bc )
+    rlLog "Hooks ended in $t seconds"
+}
+
 function generate_crash() {
     rlLog "Generate crash"
     will_segfault
@@ -25,4 +64,9 @@ function generate_crash() {
 function generate_second_crash() {
     rlLog "Generate second crash"
     will_abort
+}
+
+function prepare() {
+    rm -f /var/spool/abrt/last-ccpp
+    rm -f /tmp/abrt-done
 }
