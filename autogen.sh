536cf913a193f8ba4863029db82e860fdaabf095
Jakub Filak <jfilak@redhat.com>
2013-02-08 11:17:25 +0100

    autogen: add function which installs dependencies (works on Fedora)
    Signed-off-by: Jakub Filak <jfilak@redhat.com>

--- autogen.sh
+++ autogen.sh
@@ -1,29 +1,70 @@
 #! /bin/sh
-echo "Running gen-version"
-./gen-version
 
-mkdir -p m4
-echo "Creating m4/aclocal.m4 ..."
-test -r m4/aclocal.m4 || touch m4/aclocal.m4
+function print_help()
+{
+cat << EOH
+Prepares the source tree for configuration
 
-echo "Running autopoint"
-autopoint --force || exit 1
+Usage:
+  autogen.sh [sydeps [--install]]
 
-echo "Running intltoolize..."
-intltoolize --force --copy --automake || exit 1
+Options:
 
-echo "Running aclocal..."
-aclocal || exit 1
+  sysdeps          prints out all dependencies
+    --install      install all dependencies ('sudo yum install \$DEPS')
 
-echo "Running libtoolize..."
-libtoolize || exit 1
+EOH
+}
 
-echo "Running autoheader..."
-autoheader || return 1
+function build_depslist()
+{
+    DEPS_LIST=`grep "^\(Build\)\?Requires:" *.spec.in | grep -v "%{name}" | tr -s " " | tr "," "\n" | cut -f2 -d " " | grep -v "^abrt" | sort -u | tr "\n" " "`
+}
 
-echo "Running autoconf..."
-autoconf --force || exit 1
+case $1 in
+    "--help"|"-h")
+            print_help
+            exit 0
+        ;;
+    "sysdeps")
+            build_depslist
 
-echo "Running automake..."
-automake --add-missing --force --copy || exit 1
+            if [ "$2" == "--install" ]; then
+                set -x verbose
+                sudo yum install $DEPS_LIST
+                set +x verbose
+            else
+                echo $DEPS_LIST
+            fi
+            exit 0
+        ;;
+        *)
+            echo "Running gen-version"
+            ./gen-version
 
+            mkdir -p m4
+            echo "Creating m4/aclocal.m4 ..."
+            test -r m4/aclocal.m4 || touch m4/aclocal.m4
+
+            echo "Running autopoint"
+            autopoint --force || exit 1
+
+            echo "Running intltoolize..."
+            intltoolize --force --copy --automake || exit 1
+
+            echo "Running aclocal..."
+            aclocal || exit 1
+
+            echo "Running libtoolize..."
+            libtoolize || exit 1
+
+            echo "Running autoheader..."
+            autoheader || return 1
+
+            echo "Running autoconf..."
+            autoconf --force || exit 1
+
+            echo "Running automake..."
+            automake --add-missing --force --copy || exit 1
+        ;;
+esac
