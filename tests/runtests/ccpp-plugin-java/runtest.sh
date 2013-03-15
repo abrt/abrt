ffb0be4502ac453c17cc2cfb59f0d8059d56487f
Jakub Filak <jfilak@redhat.com>
2013-03-15 13:44:00 +0100

    testsuite: add tests for Java VM dump directories
    - the test just checks if the hs_err.log file is present in a dump
      directory
    - requires will_java_segfault executable from will-crash > 0.3
    
    - related to #622
    
    Signed-off-by: Jakub Filak <jfilak@redhat.com>

---
+++ tests/runtests/ccpp-plugin-java/runtest.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+# vim: dict=/usr/share/beakerlib/dictionary.vim cpt=.,w,b,u,t,i,k
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#   runtest.sh of ccpp-plugin-java
+#   Description: Tests Java VM functionality of ccpp-plugin
+#   Author: Jakub Filak <jfilak@redhat.com>
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#   Copyright (c) 2013 Red Hat, Inc. All rights reserved.
+#
+#   This copyrighted material is made available to anyone wishing
+#   to use, modify, copy, or redistribute it subject to the terms
+#   and conditions of the GNU General Public License version 2.
+#
+#   This program is distributed in the hope that it will be
+#   useful, but WITHOUT ANY WARRANTY; without even the implied
+#   warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+#   PURPOSE. See the GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public
+#   License along with this program; if not, write to the Free
+#   Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+#   Boston, MA 02110-1301, USA.
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+. /usr/share/beakerlib/beakerlib.sh
+. ../aux/lib.sh
+
+TEST="ccpp-plugin-java"
+PACKAGE="abrt"
+
+
+rlJournalStart
+    rlPhaseStartSetup
+        check_prior_crashes
+
+        old_ulimit=$(ulimit -c)
+        rlRun "ulimit -c unlimited" 0
+
+        TmpDir=$(mktemp -d)
+        pushd $TmpDir
+    rlPhaseEnd
+
+    rlPhaseStartTest
+        will_java_segfault
+        get_crash_path
+        wait_for_hooks
+
+        ls $crash_PATH > crash_dir_ls
+
+        rlAssertExists "$crash_PATH/hs_err.log"
+    rlPhaseEnd
+
+    rlPhaseStartCleanup
+        rlRun "abrt-cli rm $crash_PATH" 0 "Remove crash directory"
+        rlRun "ulimit -c $old_ulimit" 0
+        rlBundleLogs abrt $(echo *_ls)
+        popd # TmpDir
+        rm -rf $TmpDir
+    rlPhaseEnd
+    rlJournalPrintText
+rlJournalEnd
