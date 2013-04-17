97c76d9a43f1cfebe7d3bc222882d94f0b426319
Denys Vlasenko <dvlasenk@redhat.com>
2013-04-17 12:56:11 +0200

    Fix rhel6 version of verify-that-report-edits test
    Signed-off-by: Denys Vlasenko <dvlasenk@redhat.com>

--- tests/runtests/distro_specific/rhel6/verify-that-report-edits/runtest.sh
+++ tests/runtests/distro_specific/rhel6/verify-that-report-edits/runtest.sh
@@ -26,43 +26,69 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 . /usr/share/beakerlib/beakerlib.sh
-. ../../../aux/lib.sh
 
 TEST="verify-that-report-edits"
 PACKAGE="abrt"
 
 rlJournalStart
     rlPhaseStartSetup
-        check_prior_crashes
-
-        TmpDir=$(mktemp -d)
         rlRun "yum -y install expect" 0 "Install expect"
+        TmpDir=$(mktemp -d)
         cp "./fakeditor.sh" $TmpDir
         cp "./expect" $TmpDir
         pushd $TmpDir
-
-        generate_crash
-        wait_for_hooks
-        get_crash_path
+        rlLog "Generate crash"
+        sleep 3m &
+        sleep 1
+        kill -SIGSEGV %1
+        # Give post-create (if any) a bit of time to finish working:
+        sleep 5
+        rlLog "abrt-cli: $(abrt-cli list -f)"
+        crash_PATH=$(abrt-cli list -f | grep Directory | tail -n1 | awk '{ print $2 }')
+        if [ ! -d "$crash_PATH" ]; then
+            rlDie "No crash dir generated, this shouldn't happen"
+        fi
+        rlLog "crash_PATH=$crash_PATH"
     rlPhaseEnd
 
     rlPhaseStartTest
-        rlRun "./expect $crash_PATH &> out" 0 "Running abrt-cli via expect"
-        rlLog "$( cat out)"
+        # Make uploader send tarball not to /tmp, but to our temp dir,
+        # with fixed file name.
+        Upload_URL="file://$TmpDir/out.tar.gz"
+        export Upload_URL
+        # This is the fragile part.
+        # Expect must navigate us through all possible dialogs abrt-cli
+        # might throw at us. This doesn't work stable enough for me :(
+        # FIXME.
+        rlRun "./expect $crash_PATH &>out" 0 "Running abrt-cli via expect"
+        rlLog "$(cat out)"
+        # Check the results
         rlAssertGrep "The report has been updated" out
-
-        smart_quote="$(grep 'smart_quote=' fakeditor.sh | awk -F '"' '{ print $2 }')"
         rlAssertGrep "edited successfully" out
-        rlAssertGrep "/tmp/abrt.log" out
         rlAssertNotGrep "!! Timeout !!" out
+        smart_quote="$(grep 'smart_quote=' fakeditor.sh | awk -F '"' '{ print $2 }')"
         rlLog "smart_quote=$smart_quote"
-        rlAssertGrep "$smart_quote" "/tmp/abrt.log"
+        #
+        # We used to invoke Logger, but now the default config makes "abrt-cli report"
+        # to show these choices:
+        # 1) New Red Hat Support case
+        # 2) Existing Red Hat Support case
+        # 3) Save to tar archive
+        #
+        # ./expect script (tries to) invoke choice (3)
+        #
+        #rlAssertGrep "/tmp/abrt.log" out
+        #rlAssertGrep "$smart_quote" "/tmp/abrt.log"
+        rlAssertGrep "Successfully sent .* to $Upload_URL" out
+        rlRun "tar xf $TmpDir/out.tar.gz -O comment | grep -qF '$smart_quote'" \
+                0 "Comment is edited in saved tarball"
     rlPhaseEnd
 
     rlPhaseStartCleanup
         rlRun "abrt-cli rm $crash_PATH" 0 "Delete $crash_PATH"
         popd # TmpDir
-        rm -rf $TmpDir
+        # Comment next line out, and examine $TmpDir contents when debugging:
+        rlRun "rm -rf $TmpDir" 0 "Removing tmp directory"
     rlPhaseEnd
     rlJournalPrintText
 rlJournalEnd
