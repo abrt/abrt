603ea6539af4a26f22e01f6ba8a1b104c2aaba60
Jakub Filak <jfilak@redhat.com>
2013-05-31 12:06:27 +0200

    testsuite: add test order for el7
    We must disable bodhi on RHEL because it is usable only for Fedora
    
    Signed-off-by: Jakub Filak <jfilak@redhat.com>
    Signed-off-by: Richard Marko <rmarko@redhat.com>

--- tests/helpers/targets/el7/defs.sh
+++ tests/helpers/targets/el7/defs.sh
@@ -3,7 +3,7 @@
 DATA_DIR=~/targets/el7
 VM_NAME='EL7_nightly_run'
 REPO='git://abrt.brq.redhat.com/abrt.git'
-OS_VARIANT='rhel6'
+OS_VARIANT='rhel7'
 KS_NAME_PREFIX='brq_el7'
 LOC='http://download.englab.brq.redhat.com/pub/rhel/nightly/latest-RHEL-7/compose/Workstation/x86_64/os/'
 DISK=$( echo /dev/mapper/*el7_vm )
