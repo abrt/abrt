d2517f22d9ba810cabb5c85fffe2ff202a9d222f
Denys Vlasenko <dvlasenk@redhat.com>
2013-07-26 11:23:56 +0200

    abrt-hook-ccpp: send "POST /creation_notification" after creating problem dir
    Signed-off-by: Denys Vlasenko <dvlasenk@redhat.com>
    Signed-off-by: Martin Milata <mmilata@redhat.com>

--- src/hooks/abrt-hook-ccpp.c
+++ src/hooks/abrt-hook-ccpp.c
@@ -19,6 +19,7 @@
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 #include <syslog.h>
+#include <sys/un.h>
 #include <sys/utsname.h>
 #include "libabrt.h"
 
@@ -456,6 +457,38 @@ static int create_or_die(const char *filename)
     perror_msg_and_die("Can't open '%s'", filename);
 }
 
+static void notify_new_path(const char *path)
+{
+    int fd = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (fd < 0)
+    {
+        perror_msg("socket(AF_UNIX)");
+        return;
+    }
+
+    struct sockaddr_un sunx;
+    memset(&sunx, 0, sizeof(sunx));
+    sunx.sun_family = AF_UNIX;
+    strcpy(sunx.sun_path, VAR_RUN"/abrt/abrt.socket");
+
+    if (connect(fd, (struct sockaddr *)&sunx, sizeof(sunx)))
+    {
+        perror_msg("connect('%s')", sunx.sun_path);
+        close(fd);
+        return;
+    }
+
+    full_write_str(fd, "POST /creation_notification HTTP/1.1\r\n\r\n");
+    full_write_str(fd, path);
+    /*
+     * This sends FIN packet. Without it, close() may result in RST instead.
+     * Not really needed on AF_UNIX, just a bit of TCP-induced paranoia
+     * aka "good practice".
+     */
+    shutdown(fd, SHUT_WR);
+    close(fd);
+}
+
 int main(int argc, char** argv)
 {
     /* Kernel starts us with all fd's closed.
@@ -838,6 +871,8 @@ int main(int argc, char** argv)
         free(newpath);
 
         log("Saved core dump of pid %lu (%s) to %s (%llu bytes)", (long)pid, executable, path, (long long)core_size);
+
+        notify_new_path(path);
 
         /* rhbz#539551: "abrt going crazy when crashing process is respawned" */
         if (g_settings_nMaxCrashReportsSize > 0)
