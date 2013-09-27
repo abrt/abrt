84119036248105c615acc645ffa4dafa1f8d0b9d
Petr Kubat <pkubat@redhat.com>
2013-09-27 09:48:43 +0200

    testsuite: incomplete problem data recognition test - closes #639
    Signed-off-by: Petr Kubat <pkubat@redhat.com>

---
+++ tests/runtests/incomplete-problem/runtest.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+# vim: dict=/usr/share/beakerlib/dictionary.vim cpt=.,w,b,u,t,i,k
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#   runtest.sh of incomplete-problem
+#   Description: Tests recognition of incomplete problem data.
+#   Author: Petr Kubat <pkubat@redhat.com>
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#   Copyright (c) 2013 Red Hat, Inc. All rights reserved.
+#
+#   This program is free software: you can redistribute it and/or
+#   modify it under the terms of the GNU General Public License as
+#   published by the Free Software Foundation, either version 3 of
+#   the License, or (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be
+#   useful, but WITHOUT ANY WARRANTY; without even the implied
+#   warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+#   PURPOSE.  See the GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program. If not, see http://www.gnu.org/licenses/.
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+. /usr/share/beakerlib/beakerlib.sh
+. ../aux/lib.sh
+
+TEST="incomplete-problem"
+PACKAGE="abrt"
+
+rlJournalStart
+    rlPhaseStartSetup
+        rlShowRunningKernel
+        load_abrt_conf
+    rlPhaseEnd
+
+    rlPhaseStartTest
+        rlLog "Creating problem data."
+        dbus-send --system --type=method_call --print-reply \
+          --dest=org.freedesktop.problems /org/freedesktop/problems \
+          org.freedesktop.problems.NewProblem \
+          dict:string:string:analyzer,libreport,reason,"Testing crash",backtrace,"die()",executable,"/usr/bin/true"
+        cd /var/tmp/abrt/libreport*
+        rlLog "Waiting few seconds."
+        sleep 2s
+        rlLog "Removing count file and restarting abrtd"
+        rm -f count
+        systemctl restart abrtd.service
+        sleep 2s
+        rlLog "Abrtd should recognize the problem and add a 'not-reportable file'"
+        rlAssertExists not-reportable
+    rlPhaseEnd
+
+    rlPhaseStartCleanup
+        rlRun "rm -rf /var/tmp/abrt/libreport*" 0 "Removing problem dir"
+    rlPhaseEnd
+    rlJournalPrintText
+rlJournalEnd
