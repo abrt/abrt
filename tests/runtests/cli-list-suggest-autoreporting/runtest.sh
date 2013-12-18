0cd2d4640138e2d4c0a93d74b0a7e1e2cbe0298a
Jakub Filak <jfilak@redhat.com>
2013-12-18 17:30:32 +0100

    testsuite: test presence of the auto-reporting suggestion text
    Related to rhbz#104442
    
    Signed-off-by: Jakub Filak <jfilak@redhat.com>

---
+++ tests/runtests/cli-list-suggest-autoreporting/runtest.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+# vim: dict=/usr/share/beakerlib/dictionary.vim cpt=.,w,b,u,t,i,k
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#   runtest.sh of cli-list-suggests-autoreporting
+#   Description: checks whether abrt-cli list encourages users to enable auto-reporting
+#   Author: Jakub Filak <jfilak@redhat.com>
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#   Copyright (c) 2014 Red Hat, Inc. All rights reserved.
+#
+#   This program is free software: you can redistribute it and/or
+#   modify it under the terms of the GNU General Public License as
+#   published by the Free Software Foundation, either version 3 of
+#   the License, or (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be
+#   useful, but WITHOUT ANY WARRANTY; without even the implied
+#   warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+#   PURPOSE.  See the GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program. If not, see http://www.gnu.org/licenses/.
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+. /usr/share/beakerlib/beakerlib.sh
+. ../aux/lib.sh
+
+TEST="cli-list-suggests-autoreporting"
+PACKAGE="abrt"
+
+rlJournalStart
+    rlPhaseStartSetup
+        TmpDir=$(mktemp -d)
+        pushd $TmpDir
+    rlPhaseEnd
+
+    rlPhaseStartTest "auto-reporting disabled"
+        rlRun "abrt-auto-reporting disabled"
+        rlRun "abrt-cli list 2>&1 | grep 'abrt-auto-reporting enabled'"
+    rlPhaseEnd
+
+    rlPhaseStartTest "auto-reporting enabled"
+        rlRun "abrt-auto-reporting enabled"
+        rlRun "abrt-cli list 2>&1 | grep -v 'abrt-auto-reporting enabled'"
+    rlPhaseEnd
+
+    rlPhaseStartCleanup
+        popd # TmpDir
+        rm -rf $TmpDir
+    rlPhaseEnd
+    rlJournalPrintText
+rlJournalEnd
+
