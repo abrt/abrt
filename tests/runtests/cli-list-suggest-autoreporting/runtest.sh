824bc34e36f20c215a3bc5aeb65cc8298560df45
Richard Marko <rmarko@redhat.com>
2014-01-14 14:18:34 +0100

    testsuite: fix autoreporting notification test
    Signed-off-by: Richard Marko <rmarko@redhat.com>

--- tests/runtests/cli-list-suggest-autoreporting/runtest.sh
+++ tests/runtests/cli-list-suggest-autoreporting/runtest.sh
@@ -39,12 +39,14 @@ rlJournalStart
 
     rlPhaseStartTest "auto-reporting disabled"
         rlRun "abrt-auto-reporting disabled"
-        rlRun "abrt-cli list 2>&1 | grep 'abrt-auto-reporting enabled'"
+        rlRun "abrt-cli list &> out"
+        rlAssertGrep 'abrt-auto-reporting enabled' out
     rlPhaseEnd
 
     rlPhaseStartTest "auto-reporting enabled"
         rlRun "abrt-auto-reporting enabled"
-        rlRun "abrt-cli list 2>&1 | grep -v 'abrt-auto-reporting enabled'"
+        rlRun "abrt-cli list &> out"
+        rlAssertNotGrep 'enabled' out
     rlPhaseEnd
 
     rlPhaseStartCleanup
