e563de7f55036ec4a036161157f172beea15010a
Jakub Filak <jfilak@redhat.com>
2014-08-07 21:41:21 +0200

    json: include json.h accordingly to json-c CFLAGS
    We use $(pkg-config --cflags json-c) but we include <json/json.h>. The
    proper way to include the json-c header is to include <json.h>.
    
    That absolute include causes errors while compiling with json-c-0.12
    where the base include directory was changed  to "json-c".
    
    https://github.com/json-c/json-c/blob/master/ChangeLog
    
    Signed-off-by: Jakub Filak <jfilak@redhat.com>

--- src/plugins/bodhi.c
+++ src/plugins/bodhi.c
@@ -17,7 +17,7 @@
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
-#include <json/json.h>
+#include <json.h>
 #include <rpm/rpmts.h>
 #include <rpm/rpmcli.h>
 #include <rpm/rpmdb.h>
