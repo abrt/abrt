5e53150d17062f46585ea88bced56e8895a0ffc4
Jakub Filak <jfilak@redhat.com>
2015-02-27 08:50:16 +0100

    console-notofications: robustidize the implementation
    Signed-off-by: Jakub Filak <jfilak@redhat.com>

--- src/cli/abrt-console-notification.sh
+++ src/cli/abrt-console-notification.sh
@@ -1,22 +1,42 @@
+# If shell is not connect to a terminal, return immediately, because this script
+# should print out ABRT's status and it is senseless to continue without
+# terminal.
+tty -s || return 0
+
+# Skip all for noninteractive shells for the same reason as above.
+[ -z "$PS1" ] && return 0
+
+# If $HOME is not set, a non human user is logging in to shell but this script
+# should provide information to human users, therefore returning immediately
+# without showing the notification.
+if [ -z "$HOME" ]; then
+    return 0
+fi
+
+if [ -z "$ABRT_DEBUG_LOG" ]; then
+    ABRT_DEBUG_LOG="/dev/null"
+fi
+
 LPATHDIR="$HOME/.cache/abrt"
 SINCEFILE="$LPATHDIR/lastnotification"
 
 if [ ! -f "$LPATHDIR" ]; then
-    mkdir -p "$LPATHDIR"
+    # It might happen that user doesn't have write access on his home.
+    mkdir -p "$LPATHDIR" >"$ABRT_DEBUG_LOG" 2>&1 || return 0
 fi
 
-TMPPATH=`mktemp --tmpdir="$LPATHDIR" lastnotification.XXXXXXXX 2> /dev/null`
+TMPPATH=`mktemp --tmpdir="$LPATHDIR" lastnotification.XXXXXXXX 2> "$ABRT_DEBUG_LOG"`
 
 SINCE=0
 if [ -f "$SINCEFILE" ]; then
-    SINCE=`cat $SINCEFILE 2> /dev/null`
+    SINCE=`cat $SINCEFILE 2>"$ABRT_DEBUG_LOG"`
 fi
 
 # always update the lastnotification
 if [ -f "$TMPPATH" ]; then
-    date +%s > "$TMPPATH"
-    mv -f "$TMPPATH" "$SINCEFILE"
+    # Be quite in case of errors and don't scare users by strange error messages.
+    date +%s > "$TMPPATH" 2>"$ABRT_DEBUG_LOG"
+    mv -f "$TMPPATH" "$SINCEFILE" >"$ABRT_DEBUG_LOG" 2>&1
 fi
 
-abrt-cli status --since="$SINCE" 2> /dev/null
-
+timeout 10s abrt-cli status --since="$SINCE" 2>"$ABRT_DEBUG_LOG" || echo "'abrt-cli status' timed out"
