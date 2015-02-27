c1d060f8c19239412af0cfa51414db2435b6782b
Jakub Filak <jfilak@redhat.com>
2015-02-27 15:10:29 +0100

    ureport: rename the event to submit_uReport
    Because we don't want to show the event in report-gtk in the list of available
    reporters (report-gtk includes all events prefixed with 'report' in that list).
    
    Signed-off-by: Jakub Filak <jfilak@redhat.com>

--- src/lib/abrt_conf.c
+++ src/lib/abrt_conf.c
@@ -90,7 +90,7 @@ static void ParseCommon(map_string_t *settings, const char *conf_filename)
         g_hash_table_remove(settings, "AutoreportingEvent");
     }
     else
-        g_settings_autoreporting_event = xstrdup("report_uReport");
+        g_settings_autoreporting_event = xstrdup("submit_uReport");
 
     GHashTableIter iter;
     const char *name;
