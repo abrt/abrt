cdb4c5b0855d910132e61d71afbd445b0271fcb4
Jakub Filak <jfilak@redhat.com>
2015-04-21 07:54:17 +0200

    ccpp: avoid overriding system files by coredump
    Related: #1211835
    
    Signed-off-by: Jakub Filak <jfilak@redhat.com>

--- src/hooks/abrt-hook-ccpp.c
+++ src/hooks/abrt-hook-ccpp.c
@@ -544,7 +544,7 @@ static bool dump_fd_info(const char *dest_filename, char *source_filename, int s
 /* Like xopen, but on error, unlocks and deletes dd and user core */
 static int create_or_die(const char *filename)
 {
-    int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, DEFAULT_DUMP_DIR_MODE);
+    int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC | O_EXCL, DEFAULT_DUMP_DIR_MODE);
     if (fd >= 0)
     {
         IGNORE_RESULT(fchown(fd, dd->dd_uid, dd->dd_gid));
