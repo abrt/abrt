0b8955a845f7a28a090bfe07d33a0acf161526e2
Matej Habrnal <mhabrnal@redhat.com>
2015-04-24 15:37:15 +0200

    abrt-auto-reporting: require rhtsupport.conf file only on RHEL
    abrt-auto-reporting required the rhtsupport.conf on Fedora and CentOS but the conf
    file do not exists in those systems.
    
    Resolves abrt/abrt#957
    
    Signed-off-by: Matej Habrnal <mhabrnal@redhat.com>

--- src/daemon/abrt-auto-reporting.c
+++ src/daemon/abrt-auto-reporting.c
@@ -64,6 +64,7 @@ set_abrt_reporting(map_string_t *conf, const char *opt_value)
     return 1;
 }
 
+#if AUTHENTICATED_AUTOREPORTING != 0
 static int
 set_ureport_http_auth(map_string_t *conf, const char *opt_value)
 {
@@ -134,6 +135,7 @@ set_rhts_credentials(map_string_t *conf, const char *username, const char *passw
     /* No changes needed -> success */
     return 1;
 }
+#endif
 
 static const char *
 get_abrt_reporting(map_string_t *conf)
@@ -143,6 +145,7 @@ get_abrt_reporting(map_string_t *conf)
     return REPORTING_STATES[index][0];
 }
 
+#if AUTHENTICATED_AUTOREPORTING != 0
 static const char *
 get_ureport_http_auth(map_string_t *conf)
 {
@@ -154,6 +157,7 @@ get_ureport_client_auth(map_string_t *conf)
 {
     return get_map_string_item_or_NULL(conf, UREPORT_CLIENT_AUTH_OPTION);
 }
+#endif
 
 int main(int argc, char *argv[])
 {
@@ -171,57 +175,78 @@ int main(int argc, char *argv[])
     textdomain(PACKAGE);
 #endif
 
+#define PROGRAM_USAGE_MIDDLE_PART \
+            "\n" \
+            "Get or modify a value of the auto-reporting option. The changes will take\n" \
+            "effect immediately and will be persistent.\n" \
+            "\n" \
+            ""STATE_MANUAL":\n" \
+            "User have to report the detect problems manually\n" \
+            "\n" \
+            ""STATE_AUTO":\n" \
+            "ABRT uploads an uReport which was generated for a detected problem\n" \
+            "immediately after the detection phase. uReport generally contains a stack\n" \
+            "trace which only describes the call stack of the program at the time of the\n" \
+            "crash and does not contain contents of any variables.  Every uReport also\n" \
+            "contains identification of the operating system, versions of the RPM packages\n" \
+            "involved in the crash, and whether the program ran under a root user.\n" \
+            "\n"
+
     abrt_init(argv);
+#if AUTHENTICATED_AUTOREPORTING != 0
     const char *program_usage_string = _(
             "& [ "STATE_MANUAL" | "STATE_AUTO" | yes | no | 1 | 0 ] \\\n"
             "  [[--anonymous] | [--username USERNAME [--password PASSWORD]] | [--certificate SOURCE]]\n"
-            "\n"
-            "Get or modify a value of the auto-reporting option. The changes will take\n"
-            "effect immediately and will be persistent.\n"
-            "\n"
-            ""STATE_MANUAL":\n"
-            "User have to report the detect problems manually\n"
-            "\n"
-            ""STATE_AUTO":\n"
-            "ABRT uploads an uReport which was generated for a detected problem\n"
-            "immediately after the detection phase. uReport generally contains a stack\n"
-            "trace which only describes the call stack of the program at the time of the\n"
-            "crash and does not contain contents of any variables.  Every uReport also\n"
-            "contains identification of the operating system, versions of the RPM packages\n"
-            "involved in the crash, and whether the program ran under a root user.\n"
-            "\n"
+            PROGRAM_USAGE_MIDDLE_PART
             "See abrt-auto-reporting(1), reporter-ureport(1) and reporter-rhtsupport(1)\n"
             "for more details.\n"
     );
+#else
+    const char *program_usage_string = _(
+            "& [ "STATE_MANUAL" | "STATE_AUTO" | yes | no | 1 | 0 ]\n"
+            PROGRAM_USAGE_MIDDLE_PART
+            "See abrt-auto-reporting(1) and reporter-ureport(1) for more details.\n"
+    );
+#endif
 
     enum {
         OPT_v = 1 << 0,
+#if AUTHENTICATED_AUTOREPORTING != 0
         OPT_a = 1 << 1,
         OPT_u = 1 << 2,
         OPT_p = 1 << 3,
         OPT_c = 1 << 4,
+#endif
     };
 
+#if AUTHENTICATED_AUTOREPORTING != 0
     int anonymous = 0;
     const char *username = NULL;
     const char *password = NULL;
     const char *certificate = NULL;
+#endif
 
     /* Keep enum above and order of options below in sync! */
     struct options program_options[] = {
         OPT__VERBOSE(&g_verbose),
+#if AUTHENTICATED_AUTOREPORTING != 0
         OPT_BOOL  (  'a', "anonymous",   &anonymous,               _("Turns the authentication off")),
         OPT_STRING(  'u', "username",    &username,    "USERNAME", _("Red Hat Support user name")),
         OPT_STRING(  'p', "password",    &password,    "PASSWORD", _("Red Hat Support password, if not given, a prompt for it will be issued")),
         OPT_STRING(  'c', "certificate", &certificate, "SOURCE",   _("uReport SSL certificate paths or certificate type")),
+#endif
         OPT_END()
     };
 
-    const unsigned opts = parse_opts(argc, argv, program_options, program_usage_string);
+#if AUTHENTICATED_AUTOREPORTING != 0
+    const unsigned opts =
+#endif
+    parse_opts(argc, argv, program_options, program_usage_string);
 
     argv += optind;
     argc -= optind;
 
+#if AUTHENTICATED_AUTOREPORTING != 0
     if ((opts & OPT_p) && !(opts & OPT_u))
     {
         error_msg(_("You also need to specify --username for --password"));
@@ -246,6 +271,7 @@ int main(int argc, char *argv[])
         show_usage_and_die(program_usage_string, program_options);
     }
 
+#endif
     if (argc > 1)
     {
         error_msg(_("Invalid number of arguments"));
@@ -275,20 +301,25 @@ int main(int argc, char *argv[])
     int exit_code = EXIT_FAILURE;
 
     map_string_t *conf = new_map_string();
+#if AUTHENTICATED_AUTOREPORTING != 0
     map_string_t *rhts_conf = new_map_string();
     map_string_t *rhts_conf_bck = NULL;
+#endif
     map_string_t *ureport_conf = new_map_string();
     map_string_t *ureport_conf_bck = NULL;
 
     if (!load_abrt_conf_file(CONF_NAME, conf))
         goto finito;
 
+#if AUTHENTICATED_AUTOREPORTING != 0
     if (!load_plugin_conf_file(RHTS_NAME, rhts_conf, false))
         goto finito;
+#endif
 
     if (!load_plugin_conf_file(UREPORT_NAME, ureport_conf, false))
         goto finito;
 
+#if AUTHENTICATED_AUTOREPORTING != 0
     if ((opts & OPT_a))
     {
         ureport_conf_bck = clone_map_string(ureport_conf);
@@ -334,11 +365,13 @@ int main(int argc, char *argv[])
             goto finito;
     }
 
+#endif
     if (argc == 0)
     {
         printf("%s", get_abrt_reporting(conf));
         exit_code = EXIT_SUCCESS;
 
+#if AUTHENTICATED_AUTOREPORTING != 0
         if (g_verbose >= 1)
         {
             const char *tmp = get_ureport_http_auth(ureport_conf);
@@ -350,7 +383,7 @@ int main(int argc, char *argv[])
             else
                 printf(" %s", _("anonymous auto reporting"));
         }
-
+#endif
         putchar('\n');
 
         goto finito;
@@ -363,16 +396,20 @@ int main(int argc, char *argv[])
         if (ureport_conf_bck != NULL)
             save_plugin_conf_file(UREPORT_NAME, ureport_conf_bck);
 
+#if AUTHENTICATED_AUTOREPORTING != 0
         if (rhts_conf_bck != NULL)
             save_plugin_conf_file(RHTS_NAME, rhts_conf_bck);
+#endif
     }
 
 
 finito:
     free_map_string(ureport_conf);
     free_map_string(ureport_conf_bck);
+#if AUTHENTICATED_AUTOREPORTING != 0
     free_map_string(rhts_conf);
     free_map_string(rhts_conf_bck);
+#endif
     free_map_string(conf);
     return exit_code;
 }
