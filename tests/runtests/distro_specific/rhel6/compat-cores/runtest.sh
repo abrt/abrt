784dc5ad6c86b58019b56b90f9afef3d57f01831
Martin Kyral <mkyral@redhat.com>
2015-06-09 16:58:13 +0200

    testsuite: clone compat-cores for rhel6
    Signed-off-by: Jakub Filak <jfilak@redhat.com>

--- tests/runtests/distro_specific/rhel6/compat-cores/runtest.sh
+++ tests/runtests/distro_specific/rhel6/compat-cores/runtest.sh
@@ -27,7 +27,7 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 . /usr/share/beakerlib/beakerlib.sh
-. ../aux/lib.sh
+. ../../../aux/lib.sh
 
 TEST="compat-cores"
 PACKAGE="abrt"
@@ -92,6 +92,8 @@ rlJournalStart
         rlLog "Create an artificial core dump file."
         rlRun "touch core.$killpid"
 
+        OLDCOUNT=$(grep "Can't open.*core.$killpid.*at '.*': Permission denied" /var/log/messages | wc -l)
+
         rlRun "kill -SIGSEGV $killpid"
 
         wait_for_hooks
@@ -99,7 +101,8 @@ rlJournalStart
 
         rlLog "core: `ls -l`"
         rlAssertEquals "Checking if core has 0 Bytes" "x$(ls -l core.$killpid | cut -f5 -d' ')" "x0"
-        rlRun "journalctl SYSLOG_IDENTIFIER=abrt-hook-ccpp --since=\"$SINCE\" | grep \"Can't open.*core.$killpid.*at '.*': Permission denied\""
+        NEWCOUNT=$(grep "Can't open.*core.$killpid.*at '.*': Permission denied" /var/log/messages | wc -l)
+        rlAssertEquals "logged: Can't open.*core.$killpid.*at '.*': Permission denied" $(($OLDCOUNT+1)) $NEWCOUNT
 
         rlRun "abrt-cli rm $crash_PATH"
         rlRun "rm core.$killpid"
@@ -121,6 +124,8 @@ rlJournalStart
         rlRun "touch core.$killpid"
         rlRun "chown abrt-suid-test:abrt-suid-test core.$killpid"
 
+        OLDCOUNT=$(grep ".*core.$killpid.* at '.*' is not a regular file with link count 1 owned by UID(0)" /var/log/messages | wc -l)
+
         rlRun "kill -SIGSEGV $killpid"
 
         wait_for_hooks
@@ -128,7 +133,8 @@ rlJournalStart
 
         rlLog "core: `ls -l`"
         rlAssertEquals "Checking if core has 0 Bytes" "x$(ls -l core.$killpid | cut -f5 -d' ')" "x0"
-        rlRun "journalctl SYSLOG_IDENTIFIER=abrt-hook-ccpp --since=\"$SINCE\" | grep \".*core.$killpid.* at '.*' is not a regular file with link count 1 owned by UID(0)\""
+        NEWCOUNT=$(grep ".*core.$killpid.* at '.*' is not a regular file with link count 1 owned by UID(0)" /var/log/messages | wc -l)
+        rlAssertEquals "logged: .*core.$killpid.* at '.*' is not a regular file..." $(($OLDCOUNT+1)) $NEWCOUNT
 
         rlRun "abrt-cli rm $crash_PATH"
         rlRun "rm core.$killpid"
@@ -150,6 +156,8 @@ rlJournalStart
         rlRun "echo all > denied.conf"
         rlRun "ln denied.conf core.$killpid"
 
+        OLDCOUNT=$(grep ".*core.$killpid.* at '.*' is not a regular file with link count 1 owned by UID(0)" /var/log/messages | wc -l)
+
         rlRun "kill -SIGSEGV $killpid"
 
         wait_for_hooks
@@ -157,7 +165,8 @@ rlJournalStart
 
         rlLog "core: `ls -l`"
         rlAssertEquals "Checking if denied.conf is untouched" "x$(cat denied.conf)" "xall"
-        rlRun "journalctl SYSLOG_IDENTIFIER=abrt-hook-ccpp --since=\"$SINCE\" | grep \".*core.$killpid.* at '.*' is not a regular file with link count 1 owned by UID(0)\""
+        NEWCOUNT=$(grep ".*core.$killpid.* at '.*' is not a regular file with link count 1 owned by UID(0)" /var/log/messages | wc -l)
+        rlAssertEquals "logged: .*core.$killpid.* at '.*' is not a regular file..." $(($OLDCOUNT+1)) $NEWCOUNT
 
         rlRun "abrt-cli rm $crash_PATH"
         rlRun "rm denied.conf core.$killpid"
@@ -175,6 +184,9 @@ rlJournalStart
         killpid=`pidof $SUIDEDEXE`
         c=0;while [ ! -n "$killpid" ]; do sleep 0.1; killpid=`pidof $SUIDEDEXE`; let c=$c+1; if [ $c -gt 500 ]; then break; fi; done;
         SINCE=$(date +"%Y-%m-%d %T")
+
+        OLDCOUNT=$(grep "Current suid_dumpable policy prevents from saving core dumps according to relative core_pattern" /var/log/messages | wc -l)
+
         rlRun "kill -SIGSEGV $killpid"
 
         wait_for_hooks
@@ -182,7 +194,8 @@ rlJournalStart
 
         rlLog "core: `ls -l`"
         rlAssertNotExist 'core.$killpid' 0 "Checking if core does not exist"
-        rlRun "journalctl SYSLOG_IDENTIFIER=abrt-hook-ccpp --since=\"$SINCE\" | grep \"Current suid_dumpable policy prevents from saving core dumps according to relative core_pattern\""
+        NEWCOUNT=$(grep "Current suid_dumpable policy prevents from saving core dumps according to relative core_pattern" /var/log/messages | wc -l)
+        rlAssertEquals "logged: Current suid_dumpable policy prevents from saving core dumps according to relative core_pattern" $(($OLDCOUNT+1)) $NEWCOUNT
 
         rlRun "echo 0 > /proc/sys/fs/suid_dumpable" 0 "Set setuid no dump"
         rlRun "abrt-cli rm $crash_PATH"
@@ -194,14 +207,14 @@ rlJournalStart
         rlLog "Use absolute path in core_pattern"
         rlRun "SAVED_CORE=\"$(cat /var/run/abrt/saved_core_pattern)\""
         rlRun "CURRENT_CORE=\"$(cat /proc/sys/kernel/core_pattern)\""
-        rlRun "systemctl stop abrt-ccpp"
+        rlRun "service abrt-ccpp stop"
         rlRun "OLD_CORE=\"$(cat /proc/sys/kernel/core_pattern)\""
         if [ "_${SAVED_CORE}" == "_${CURRENT_CORE}" ]; then
             rlLog "Reset OLD_CORE to 'core'"
             OLD_CORE="core"
         fi
         rlRun "echo /var/tmp/core.%p > /proc/sys/kernel/core_pattern"
-        rlRun "systemctl start abrt-ccpp"
+        rlRun "service abrt-ccpp start"
 
         rlLog "ulimit: `ulimit -c`"
         rlRun "echo 2 > /proc/sys/fs/suid_dumpable" 0 "Set setuid secure dump"
@@ -216,6 +229,8 @@ rlJournalStart
         rlLog "Create an artificial core dump file."
         rlRun "touch /var/tmp/core.$killpid"
 
+        OLDCOUNT=$(grep "Can't open.*/var/tmp/core.$killpid.*at '.*': File exists" /var/log/messages | wc -l)
+
         rlRun "kill -SIGSEGV $killpid"
 
         wait_for_hooks
@@ -223,13 +238,14 @@ rlJournalStart
 
         rlLog "core: `ls -l /var/tmp/core.*`"
         rlAssertEquals "Checking if core has 0 Bytes" "x$(ls -l /var/tmp/core.$killpid | cut -f5 -d' ')" "x0"
-        rlRun "journalctl SYSLOG_IDENTIFIER=abrt-hook-ccpp --since=\"$SINCE\" | grep \"Can't open.*/var/tmp/core.$killpid.*at '.*': File exists\""
+        NEWCOUNT=$(grep "Can't open.*/var/tmp/core.$killpid.*at '.*': File exists" /var/log/messages | wc -l)
+        rlAssertEquals "logged: Can't open.*/var/tmp/core.$killpid.*at '.*': File exists" $(($OLDCOUNT+1)) $NEWCOUNT
 
         rlRun "rm /var/tmp/core.$killpid"
         rlRun "echo 0 > /proc/sys/fs/suid_dumpable" 0 "Set setuid no dump"
-        rlRun "systemctl stop abrt-ccpp"
+        rlRun "service abrt-ccpp stop"
         rlRun "echo \"$OLD_CORE\" > /proc/sys/kernel/core_pattern"
-        rlRun "systemctl start abrt-ccpp"
+        rlRun "service abrt-ccpp start"
         rlRun "abrt-cli rm $crash_PATH"
     rlPhaseEnd
 
@@ -239,14 +255,14 @@ rlJournalStart
         rlLog "Use absolute path in core_pattern"
         rlRun "SAVED_CORE=\"$(cat /var/run/abrt/saved_core_pattern)\""
         rlRun "CURRENT_CORE=\"$(cat /proc/sys/kernel/core_pattern)\""
-        rlRun "systemctl stop abrt-ccpp"
+        rlRun "service abrt-ccpp stop"
         rlRun "OLD_CORE=\"$(cat /proc/sys/kernel/core_pattern)\""
         if [ "_${SAVED_CORE}" == "_${CURRENT_CORE}" ]; then
             rlLog "Reset OLD_CORE to 'core'"
             OLD_CORE="core"
         fi
         rlRun "echo /var/tmp/core.%p > /proc/sys/kernel/core_pattern"
-        rlRun "systemctl start abrt-ccpp"
+        rlRun "service abrt-ccpp start"
 
         rlLog "ulimit: `ulimit -c`"
         rlRun "echo 2 > /proc/sys/fs/suid_dumpable" 0 "Set setuid secure dump"
@@ -267,9 +283,9 @@ rlJournalStart
 
         rlRun "rm /var/tmp/core.$killpid"
         rlRun "echo 0 > /proc/sys/fs/suid_dumpable" 0 "Set setuid no dump"
-        rlRun "systemctl stop abrt-ccpp"
+        rlRun "service abrt-ccpp stop"
         rlRun "echo \"$OLD_CORE\" > /proc/sys/kernel/core_pattern"
-        rlRun "systemctl start abrt-ccpp"
+        rlRun "service abrt-ccpp start"
         rlRun "abrt-cli rm $crash_PATH"
     rlPhaseEnd
 
