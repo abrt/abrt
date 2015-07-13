1a2cfcef6404983db5d4d2ddd5af824644bf1614
Matej Habrnal <mhabrnal@redhat.com>
2015-07-13 11:25:17 +0200

    a-dump-oops: allow update the problem, if more then one oops found
    In case that found more than one oops process the first one.
    Without this patch the script exits with error in this case because expects
    only one oops.
    
    Related to rhbz#1170534
    
    Signed-off-by: Matej Habrnal <mhabrnal@redhat.com>

--- src/plugins/abrt-dump-oops.c
+++ src/plugins/abrt-dump-oops.c
@@ -172,6 +172,17 @@ int main(int argc, char **argv)
         log("Updating problem directory");
         switch (g_list_length(oops_list))
         {
+            case 0:
+                {
+                    error_msg(_("Can't update the problem: no oops found"));
+                    errors = 1;
+                    break;
+                }
+            default:
+                {
+                    log_notice(_("More oopses found: process only the first one"));
+                }
+                /* falls trought */
             case 1:
                 {
                     struct dump_dir *dd = dd_opendir(problem_dir, /*open for writing*/0);
@@ -181,11 +192,6 @@ int main(int argc, char **argv)
                         dd_close(dd);
                     }
                 }
-                break;
-            default:
-                error_msg(_("Can't update the problem: more than one oops found"));
-                errors = 1;
-                break;
         }
     }
     else
