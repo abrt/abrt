182a0a74359296562098b548a3a964db273b67b0
Matej Habrnal <mhabrnal@redhat.com>
2015-09-16 15:34:59 +0200

    testsuite: make --insecure parameter optional
    There was not possible to use reporter-ureport without --insecure parameter.
    
    Signed-off-by: Matej Habrnal <mhabrnal@redhat.com>

--- tests/runtests/ureport-auth/runtest.sh
+++ tests/runtests/ureport-auth/runtest.sh
@@ -40,7 +40,7 @@ function run_reporter() {
     PYSERVE_PID=$!
     wait_for_server 12345
 
-    rlRun "reporter-ureport -vvv --insecure --url https://localhost:12345/faf -d $crash_PATH $ARGS &> ccpp_reporter" $RET "auth $AUTH, reporter-ureport $ARGS"
+    rlRun "reporter-ureport -vvv --url https://localhost:12345/faf -d $crash_PATH $ARGS &> ccpp_reporter" $RET "auth $AUTH, reporter-ureport $ARGS"
 
     kill $PYSERVE_PID
 }
@@ -61,53 +61,53 @@ rlJournalStart
 
     rlPhaseStartTest "Server auth disabled"
         # server: auth disabled, client: auth disabled
-        run_reporter none "" 70
+        run_reporter none "--insecure" 70
         rlAssertGrep "NOAUTH" server_log
 
         # server: auth disabled, client: invalid cert path
-        run_reporter none "-t /etc/doesnt.pem:/etc/exist.pem" 70
+        run_reporter none "-t /etc/doesnt.pem:/etc/exist.pem --insecure" 70
         rlAssertGrep "NOAUTH" server_log
 
         # server: auth disabled, client: untrusted cert
         # (server cert is self-signed so if used as a client cert it shouldn't be trusted)
-        run_reporter none "-t cert/server_cert.pem:cert/server_key.pem" 70
+        run_reporter none "-t cert/server_cert.pem:cert/server_key.pem --insecure" 70
         rlAssertGrep "NOAUTH" server_log
 
         # server: auth disabled, client: trusted cert
-        run_reporter none "-t cert/client_cert.pem:cert/client_key.pem" 70
+        run_reporter none "-t cert/client_cert.pem:cert/client_key.pem --insecure" 70
         rlAssertGrep "NOAUTH" server_log
     rlPhaseEnd
 
     rlPhaseStartTest "Server auth optional"
         # server: auth optional, client: auth disabled
-        run_reporter optional "" 70
+        run_reporter optional "--insecure" 70
         rlAssertGrep "NOAUTH" server_log
 
         # server: auth optional, client: invalid cert path
-        run_reporter optional "-t /etc/doesnt.pem:/etc/exist.pem" 70
+        run_reporter optional "-t /etc/doesnt.pem:/etc/exist.pem --insecure" 70
         rlAssertGrep "NOAUTH" server_log
 
         # server: auth optional, client: untrusted cert
-        run_reporter optional "-t cert/server_cert.pem:cert/server_key.pem" 70
+        run_reporter optional "-t cert/server_cert.pem:cert/server_key.pem --insecure" 70
         rlAssertGrep "NOAUTH" server_log
 
         # server: auth optional, client: trusted cert
-        run_reporter optional "-t cert/client_cert.pem:cert/client_key.pem" 70
+        run_reporter optional "-t cert/client_cert.pem:cert/client_key.pem --insecure" 70
         rlAssertGrep "AUTH ureport-reporter-cn" server_log
     rlPhaseEnd
 
     rlPhaseStartTest "Server auth required"
         # server: auth required, client: auth disabled
-        run_reporter required "" 1
+        run_reporter required "--insecure" 1
 
         # server: auth required, client: invalid cert path
-        run_reporter required "-t /etc/doesnt.pem:/etc/exist.pem" 1
+        run_reporter required "-t /etc/doesnt.pem:/etc/exist.pem --insecure" 1
 
         # server: auth required, client: untrusted cert
-        run_reporter required "-t cert/server_cert.pem:cert/server_key.pem" 1
+        run_reporter required "-t cert/server_cert.pem:cert/server_key.pem --insecure" 1
 
         # server: auth required, client: trusted cert
-        run_reporter required "-t cert/client_cert.pem:cert/client_key.pem" 70
+        run_reporter required "-t cert/client_cert.pem:cert/client_key.pem --insecure" 70
         rlAssertGrep "AUTH ureport-reporter-cn" server_log
     rlPhaseEnd
 
@@ -116,14 +116,14 @@ rlJournalStart
         CFG=/etc/libreport/plugins/ureport.conf
         mv $CFG conf_backup
         echo "SSLClientAuth = $TmpDir/cert/client_cert.pem:$TmpDir/cert/client_key.pem" > $CFG
-        run_reporter required "" 70
+        run_reporter required "--insecure" 70
         rlAssertGrep "AUTH ureport-reporter-cn" server_log
         mv conf_backup $CFG
 
         # setting certificate via environment
         uReport_SSLClientAuth="$TmpDir/cert/client_cert.pem:$TmpDir/cert/client_key.pem"
         export uReport_SSLClientAuth
-        run_reporter required "" 70
+        run_reporter required "--insecure" 70
         rlAssertGrep "AUTH ureport-reporter-cn" server_log
         uReport_SSLClientAuth=""
         export -n uReport_SSLClientAuth
@@ -136,7 +136,7 @@ rlJournalStart
             rlRun "mv $RHSM_DIR entitlement_backup"
         fi
 
-        rlRun "mkdir -p $RHSM_DIR"
+        run_reporter none "-t rhsm --insecure" 70
 
         # first go with empty $RHSM_DIR
         run_reporter none "-t rhsm" 70
@@ -153,7 +153,7 @@ rlJournalStart
         rlRun "cp cert/$RHSM_CERT $RHSM_DIR/3$RHSM_CERT"
         rlRun "cp cert/$RHSM_KEY $RHSM_DIR/3$RHSM_KEY"
 
-        run_reporter none "-t rhsm" 70
+        run_reporter none "-t rhsm --insecure" 70
 
         rlRun "cp ccpp_reporter ureport.log"
         cert=$(tr -d '\n' < cert/$RHSM_CERT)
@@ -176,15 +176,15 @@ rlJournalStart
 
     rlPhaseStartTest "HTTP Auth"
         # server: http auth required, client: auth disabled
-        run_reporter http_required "" 1
+        run_reporter http_required "--insecure" 1
         rlAssertGrep "prompted for credentials" server_log
 
         # server: http auth required, client: invalid credentials
-        run_reporter http_required "-h invalid:invalid" 1
+        run_reporter http_required "-h invalid:invalid --insecure" 1
         rlAssertGrep "invalid credentials" server_log
 
         # server: http auth required, client: valid credentials
-        run_reporter http_required "-h ureport:password" 70
+        run_reporter http_required "-h ureport:password --insecure" 70
         rlAssertGrep "HTTPAUTH OK" server_log
     rlPhaseEnd
 
@@ -196,7 +196,7 @@ rlJournalStart
         augtool set /files${CFG}/Password "password"
 
         # server: http auth required, client: rhts-credentials
-        run_reporter http_required "-h rhts-credentials" 70
+        run_reporter http_required "-h rhts-credentials --insecure" 70
         rlAssertGrep "HTTPAUTH OK" server_log
 
         mv ${CFG}_bck ${CFG}
@@ -213,7 +213,7 @@ rlJournalStart
         augtool set /files${CFG}/HTTPAuth "ureport:password"
 
         rlLog "Login and password in the ureport conf"
-        run_reporter http_required "" 70
+        run_reporter http_required "--insecure" 70
         rlAssertGrep "HTTPAUTH OK" server_log
 
         augtool set /files${CFG}/HTTPAuth "rhts-credentials"
@@ -221,7 +221,7 @@ rlJournalStart
         augtool set /files${RHTS_CFG}/Password "password"
 
         rlLog "Login and password in the rhts conf"
-        run_reporter http_required "" 70
+        run_reporter http_required "--insecure" 70
         rlAssertGrep "HTTPAUTH OK" server_log
 
         # environment variables
@@ -231,7 +231,7 @@ rlJournalStart
         export uReport_HTTPAuth
 
         rlLog "Login and password in the environment variable"
-        run_reporter http_required "" 70
+        run_reporter http_required "--insecure" 70
         rlAssertGrep "HTTPAUTH OK" server_log
 
         export -n uReport_HTTPAuth
@@ -240,7 +240,7 @@ rlJournalStart
         export uReport_HTTPAuth
 
         rlLog "'rhts-credentials' in the environment variable"
-        run_reporter http_required "" 70
+        run_reporter http_required "--insecure" 70
         rlAssertGrep "HTTPAUTH OK" server_log
 
         export -n uReport_HTTPAuth
