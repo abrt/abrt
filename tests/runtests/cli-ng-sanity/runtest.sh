98331e2ac9cbf617d42ad08c78924d28a88c358f
Matej Habrnal <mhabrnal@redhat.com>
2016-11-29 14:12:27 +0100

    testsuite: fix cli-ng-sanity test
    abrt cli-ng didn't print error message in case the problem is not
    reportable.
    
    Signed-off-by: Matej Habrnal <mhabrnal@redhat.com>

--- tests/runtests/cli-ng-sanity/runtest.sh
+++ tests/runtests/cli-ng-sanity/runtest.sh
@@ -83,8 +83,9 @@ rlJournalStart
         echo "cli_sanity_test_not_reportable" > $crash_PATH/type
         echo "cli_sanity_test_not_reportable" > $crash_PATH/analyzer
 
+        PROBLEM_ID=$(abrt list --fmt {short_id})
         rlRun "abrt report 2>&1 | tee abrt-cli-report-not-reportable.log" 0
-        rlAssertGrep "Problem '$crash_PATH' cannot be reported" abrt-cli-report-not-reportable.log
+        rlAssertGrep "Problem '$PROBLEM_ID' cannot be reported" abrt-cli-report-not-reportable.log
 
         cp -f type analyzer $crash_PATH
 
