8eaf9d9ee3a2fbe411dd76e95ab5b5941571e120
Matej Habrnal <mhabrnal@redhat.com>
2016-11-29 14:01:47 +0100

    cli-ng: fix abrt list --fmt parameter
    Argument of parameter --fmt was not used even it is defined.
    
    $ abrt list {what} raised exception UnboundLocalError.
    
    Signed-off-by: Matej Habrnal <mhabrnal@redhat.com>

--- src/cli-ng/abrtcli/cli.py
+++ src/cli-ng/abrtcli/cli.py
@@ -160,6 +160,8 @@ def list_problems(args):
 
     if not args.fmt:
         fmt = config.MEDIUM_FMT
+    else:
+        fmt = args.fmt
 
     if args.pretty != 'medium':
         fmt = getattr(config, '{}_FMT'.format(args.pretty.upper()))
