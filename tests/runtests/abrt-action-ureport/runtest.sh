1aae4d5c01026224546eac98502cae020c7a3685
Julius Milan <jmilan@redhat.com>
2017-10-19 16:48:11 +0200

    testsuite: add tests for processing of unpackaged executables

--- tests/runtests/abrt-action-ureport/runtest.sh
+++ tests/runtests/abrt-action-ureport/runtest.sh
@@ -82,7 +82,7 @@ rlJournalStart
         get_crash_path
 
 
-        rlRun "augtool rm /files/etc/libreport/plugins/ureport.conf/ContactEmail" 0 "rm ContactEmail settings frim ureport.conf"
+        rlRun "augtool rm /files/etc/libreport/plugins/ureport.conf/ContactEmail" 0 "rm ContactEmail settings from ureport.conf"
         rlRun "echo 0 > $crash_PATH/ureports_counter" 0 "set ureports_counter to 0"
 
         ./fakefaf.py &> server2.log &
@@ -106,6 +106,98 @@ rlJournalStart
         rlRun "abrt-cli rm $crash_PATH" 0 "Remove crash dir"
     rlPhaseEnd
 
+    rlPhaseStartTest "report unpackaged problem if configured"
+        prepare
+        generate_crash_unpack
+        wait_for_hooks
+        get_crash_path
+
+
+        rlLog "unset environment variable uReport_ProcessUnpackaged"
+        unset uReport_ProcessUnpackaged
+        rlRun "augtool set /files/etc/libreport/plugins/ureport.conf/ProcessUnpackaged yes" 0 "set processing of unpackaged in ureport.conf"
+        rlRun "echo 0 > $crash_PATH/ureports_counter" 0 "set ureports_counter to 0"
+
+        ./fakefaf.py &> server3.log &
+        sleep 1
+
+        pushd $crash_PATH
+        /usr/libexec/abrt-action-ureport -vvv &> $TmpDir/ureport3.log
+        popd # crash_PATH
+
+        kill %1
+
+        rlAssertGrep "curl: Connected to 127.0.0.1 (127.0.0.1) port 12345 (#0)" ureport3.log
+        rlAssertGrep "curl sent header: 'POST /faf/reports/new/ HTTP/1" ureport3.log
+
+        rlAssertGrep "Problem comes from unpackaged executable." ureport3.log
+        rlAssertNotGrep "Problem comes from unpackaged executable. Unable to create uReport." ureport3.log
+
+        rlAssertGrep ".* - - \[.*\] \"POST /faf/reports/new/ HTTP/1.1\" 202 -" server3.log
+        rlRun "abrt-cli rm $crash_PATH" 0 "Remove crash dir"
+    rlPhaseEnd
+
+    rlPhaseStartTest "report unpackaged problem if environment variable set"
+        prepare
+        generate_crash_unpack
+        wait_for_hooks
+        get_crash_path
+
+
+        rlLog "set environment variable uReport_ProcessUnpackaged"
+        export uReport_ProcessUnpackaged="yes"
+        rlRun "augtool rm /files/etc/libreport/plugins/ureport.conf/ProcessUnpackaged" 0 "unset processing of unpackaged in ureport.conf"
+        rlRun "echo 0 > $crash_PATH/ureports_counter" 0 "set ureports_counter to 0"
+
+        ./fakefaf.py &> server4.log &
+        sleep 1
+
+        pushd $crash_PATH
+        /usr/libexec/abrt-action-ureport -vvv &> $TmpDir/ureport4.log
+        popd # crash_PATH
+
+        kill %1
+
+        rlAssertGrep "curl: Connected to 127.0.0.1 (127.0.0.1) port 12345 (#0)" ureport4.log
+        rlAssertGrep "curl sent header: 'POST /faf/reports/new/ HTTP/1" ureport4.log
+
+        rlAssertGrep "Problem comes from unpackaged executable." ureport4.log
+        rlAssertNotGrep "Problem comes from unpackaged executable. Unable to create uReport." ureport4.log
+
+        rlAssertGrep ".* - - \[.*\] \"POST /faf/reports/new/ HTTP/1.1\" 202 -" server4.log
+        rlRun "abrt-cli rm $crash_PATH" 0 "Remove crash dir"
+    rlPhaseEnd
+
+    rlPhaseStartTest "do not report unpackaged problem"
+        prepare
+        generate_crash_unpack
+        wait_for_hooks
+        get_crash_path
+
+
+        rlLog "unset environment variable uReport_ProcessUnpackaged"
+        unset uReport_ProcessUnpackaged
+        rlRun "augtool rm /files/etc/libreport/plugins/ureport.conf/ProcessUnpackaged" 0 "unset processing of unpackaged in ureport.conf"
+        rlRun "echo 0 > $crash_PATH/ureports_counter" 0 "set ureports_counter to 0"
+
+        ./fakefaf.py &> server5.log &
+        sleep 1
+
+        pushd $crash_PATH
+        /usr/libexec/abrt-action-ureport -vvv &> $TmpDir/ureport5.log
+        popd # crash_PATH
+
+        kill %1
+
+        rlAssertNotGrep "curl: Connected to 127.0.0.1 (127.0.0.1) port 12345 (#0)" ureport5.log
+        rlAssertNotGrep "curl sent header: 'POST /faf/reports/new/ HTTP/1" ureport5.log
+
+        rlAssertGrep "Problem comes from unpackaged executable." ureport5.log
+        rlAsserttGrep "Problem comes from unpackaged executable. Unable to create uReport." ureport5.log
+
+        rlAssertNotGrep ".* - - \[.*\] \"POST /faf/reports/new/ HTTP/1.1\" 202 -" server5.log
+        rlRun "abrt-cli rm $crash_PATH" 0 "Remove crash dir"
+    rlPhaseEnd
 
     rlPhaseStartCleanup
         rlBundleLogs abrt $(ls server* ureport*)
---
+++ tests/runtests/abrt-crash-unpackaged/runtest.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+# vim: dict=/usr/share/beakerlib/dictionary.vim cpt=.,w,b,u,t,i,k
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#   runtest.sh of abrt-crash-unpackaged
+#   Description: Tests basic functionality of problem direrctory creation for
+#   unpackaged executables crashes.
+#   Author: Julius Milan <jmilan@redhat.com>
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#   Copyright (c) 2017 Red Hat, Inc. All rights reserved.
+#
+#   This copyrighted material is made available to anyone wishing
+#   to use, modify, copy, or redistribute it subject to the terms
+#   and conditions of the GNU General Public License version 2.
+#
+#   This program is distributed in the hope that it will be
+#   useful, but WITHOUT ANY WARRANTY; without even the implied
+#   warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+#   PURPOSE. See the GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public
+#   License along with this program; if not, write to the Free
+#   Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+#   Boston, MA 02110-1301, USA.
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+. /usr/share/beakerlib/beakerlib.sh
+. ../aux/lib.sh
+
+TEST="abrt-crash-unpackaged"
+PACKAGE="abrt"
+
+
+rlJournalStart
+    rlPhaseStartSetup
+        check_prior_crashes
+
+        TmpDir=$(mktemp -d)
+        pushd $TmpDir
+    rlPhaseEnd
+
+    rlPhaseStartTest "unpackaged binary problem dir check"
+        prepare
+        generate_crash_unpack
+        wait_for_hooks
+        get_crash_path
+
+        rlAssertNotExists $crash_PATH/component
+        rlAssertNotExists $crash_PATH/package
+        rlAssertNotExists $crash_PATH/pkg_arch
+        rlAssertNotExists $crash_PATH/pkg_epoch
+        rlAssertNotExists $crash_PATH/pkg_fingerprint
+        rlAssertNotExists $crash_PATH/pkg_name
+        rlAssertNotExists $crash_PATH/pkg_release
+        rlAssertNotExists $crash_PATH/pkg_vendor
+        rlAssertNotExists $crash_PATH/pkg_version
+
+        rlRun "abrt-cli rm $crash_PATH" 0 "Remove crash dir"
+    rlPhaseEnd
+
+    rlPhaseStartTest "packaged binary problem dir check"
+        prepare
+        generate_crash
+        wait_for_hooks
+        get_crash_path
+
+        rlAssertExists $crash_PATH/component
+        rlAssertExists $crash_PATH/package
+        rlAssertExists $crash_PATH/pkg_arch
+        rlAssertExists $crash_PATH/pkg_epoch
+        rlAssertExists $crash_PATH/pkg_fingerprint
+        rlAssertExists $crash_PATH/pkg_name
+        rlAssertExists $crash_PATH/pkg_release
+        rlAssertExists $crash_PATH/pkg_vendor
+        rlAssertExists $crash_PATH/pkg_version
+
+        rlRun "abrt-cli rm $crash_PATH" 0 "Remove crash dir"
+    rlPhaseEnd
+
+    rlPhaseStartCleanup
+        popd # TmpDir
+        rm -rf $TmpDir
+    rlPhaseEnd
+    rlJournalPrintText
+rlJournalEnd
