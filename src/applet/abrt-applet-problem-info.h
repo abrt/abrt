134b29a1d70813da32b555a6d0544bed276d55ae
Ernestas Kulik <ekulik@redhat.com>
2018-11-10 13:18:07 +0100

    applet: Port to GApplication/GNotification
    This commit ports the applet from its current use of libnotify to
    GNotification and, consequently, GApplication. That nets us persistent
    notifications following the removal of the tray icon, since currently
    they are lost when the applet goes down. Additionally, this makes the
    applet D-Bus activatable for when the notifications are clicked, so that
    the applet itself is respawned and gnome-abrt is launched. A bit
    roundabout, but that’s that.
    
    Closes https://github.com/abrt/abrt/issues/1085
    
    Signed-off-by: Ernestas Kulik <ekulik@redhat.com>

---
+++ src/applet/abrt-applet-problem-info.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <glib-object.h>
+#include <libreport/problem_data.h>
+#include <stdbool.h>
+
+#define ABRT_APPLET_TYPE_PROBLEM_INFO abrt_applet_problem_info_get_type ()
+G_DECLARE_FINAL_TYPE (AbrtAppletProblemInfo, abrt_applet_problem_info,
+                      ABRT_APPLET, PROBLEM_INFO, GObject)
+
+bool                    abrt_applet_problem_info_is_announced     (AbrtAppletProblemInfo *problem_info);
+bool                    abrt_applet_problem_info_is_foreign       (AbrtAppletProblemInfo *problem_info);
+bool                    abrt_applet_problem_info_is_packaged      (AbrtAppletProblemInfo *problem_info);
+bool                    abrt_applet_problem_info_is_reported      (AbrtAppletProblemInfo *problem_info);
+
+const char             *abrt_applet_problem_info_get_command_line (AbrtAppletProblemInfo *problem_info);
+int                     abrt_applet_problem_info_get_count        (AbrtAppletProblemInfo *problem_info);
+const char *            abrt_applet_problem_info_get_directory    (AbrtAppletProblemInfo *problem_info);
+const char            **abrt_applet_problem_info_get_environment  (AbrtAppletProblemInfo *problem_info);
+int                     abrt_applet_problem_info_get_pid          (AbrtAppletProblemInfo *problem_info);
+problem_data_t         *abrt_applet_problem_info_get_problem_data (AbrtAppletProblemInfo *problem_info);
+int                     abrt_applet_problem_info_get_time         (AbrtAppletProblemInfo *problem_info);
+
+void                    abrt_applet_problem_info_set_announced    (AbrtAppletProblemInfo *problem_info,
+                                                                   bool                   announced);
+void                    abrt_applet_problem_info_set_directory    (AbrtAppletProblemInfo *problem_info,
+                                                                   const char            *directory);
+void                    abrt_applet_problem_info_set_foreign      (AbrtAppletProblemInfo *problem_info,
+                                                                   bool                   foreign);
+void                    abrt_applet_problem_info_set_known        (AbrtAppletProblemInfo *problem_info,
+                                                                   bool                   known);
+void                    abrt_applet_problem_info_set_packaged     (AbrtAppletProblemInfo *problem_info,
+                                                                   bool                   packaged);
+void                    abrt_applet_problem_info_set_reported     (AbrtAppletProblemInfo *problem_info,
+                                                                   bool                   reported);
+
+bool                    abrt_applet_problem_info_ensure_writable  (AbrtAppletProblemInfo *problem_info);
+
+AbrtAppletProblemInfo  *abrt_applet_problem_info_new              (const char            *directory);
