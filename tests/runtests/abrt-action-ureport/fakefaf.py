abrt/abrt

commit 74966d9eb09e1e29cfdba642e44d965a20494e4c

testsuite: abrt-action-ureport: Port fakefaf to Python 3
Signed-off-by: Ernestas Kulik <ekulik@redhat.com>

tests/runtests/abrt-action-ureport/fakefaf.py
@@ -1,12 +1,13 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # Single purpose HTTP server
 # - accepts POST of ureport JSON and dumps it to a file
 
 import sys
 import json
-import BaseHTTPServer, cgi
+import cgi
+import http.server
 
-class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
+class Handler(http.server.BaseHTTPRequestHandler):
     def do_POST(self):
         # parse form data
         form = cgi.FieldStorage(
@@ -60,8 +61,8 @@ class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
         json.dump(response, self.wfile, indent=2)
 
 PORT = 12345
-print "Serving at port", PORT
+print("Serving at port", PORT)
 
 Handler.save_ureport = sys.argv[1] if len(sys.argv) > 1 else 'ureport.json'
-httpd = BaseHTTPServer.HTTPServer(("", PORT), Handler)
+httpd = http.server.HTTPServer(("", PORT), Handler)
 httpd.serve_forever()

commit c0247ff29eb75add8b50dee672ac75f8404d7e5f

testsuite: abrt-action-ureport: Fix fakefaf.py
Some fallout from 74966d9eb09e1e29cfdba642e44d965a20494e4c, responses
are written as strings, but the file stream is binary. Additionally, the
default log_message() handler does not flush stderr, resulting in empty
logs when we redirect streams to a file and run in the background.

Signed-off-by: Ernestas Kulik <ekulik@redhat.com>

tests/runtests/abrt-action-ureport/fakefaf.py
@@ -58,7 +58,12 @@ class Handler(http.server.BaseHTTPRequestHandler):
                 fh.write('{"invalid_request_path": "%s"}' % self.path)
             return
 
-        json.dump(response, self.wfile, indent=2)
+        self.wfile.write(json.dumps(response, indent=2).encode('utf-8'))
+
+    def log_message(self, format, *args):
+        super(Handler, self).log_message(format, *args)
+
+        sys.stderr.flush()
 
 PORT = 12345
 print("Serving at port", PORT)