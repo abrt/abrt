39accf6c6e5cc7f2a99359c7d43b31c874b63ba0
Ernestas Kulik <ekulik@redhat.com>
2019-08-01 14:27:54 +0200

    autogen.sh: Use autoreconf
    It’s faster, does the same and is less verbose.
    
    Signed-off-by: Ernestas Kulik <ekulik@redhat.com>

--- autogen.sh
+++ autogen.sh
@@ -53,30 +53,11 @@ case "$1" in
             echo "Running gen-version"
             ./gen-version
 
-            mkdir -p m4
-            echo "Creating m4/aclocal.m4 ..."
-            test -r m4/aclocal.m4 || touch m4/aclocal.m4
-
-            echo "Running autopoint"
-            autopoint --force || exit 1
+            echo "Running autoreconf"
+            autoreconf --install --force --verbose || exit 1
 
             echo "Running intltoolize..."
             intltoolize --force --copy --automake || exit 1
-
-            echo "Running aclocal..."
-            aclocal || exit 1
-
-            echo "Running libtoolize..."
-            libtoolize || exit 1
-
-            echo "Running autoheader..."
-            autoheader || return 1
-
-            echo "Running autoconf..."
-            autoconf --force || exit 1
-
-            echo "Running automake..."
-            automake --add-missing --force --copy || exit 1
 
             echo "Running configure ..."
             if [ 0 -eq $# ]; then
