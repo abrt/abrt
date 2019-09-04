abrt/abrt

commit 590a7a89a1e23998369fb54fa4fcaa519e0e4553

cli: Improve i18n
This commit adds the canonical short form for ngettext(), uses it for
certain strings and tweaks other strings to be easier to localize.

Signed-off-by: Ernestas Kulik <ekulik@redhat.com>

src/cli/abrtcli/cli.py
@@ -11,7 +11,7 @@ import reportclient
 from reportclient import ask_yes_no, set_verbosity
 
 from abrtcli import config, l18n
-from abrtcli.l18n import _
+from abrtcli.l18n import _, N_
 from abrtcli.match import match_completer, match_get_problem
 
 from abrtcli.filtering import (filter_not_reported,
@@ -341,7 +341,9 @@ def status(args):
         return
 
     if not args.quiet or len(probs) > 0:
-        print(_('ABRT has detected {} problem(s). For more info run: abrt list{}')
+        print(N_('ABRT has detected a problem. For more information, run “abrt list{}”',
+                 'ABRT has detected {} problems. For more information, run “abrt list{}”',
+                 len(probs))
               .format(len(probs), since_append))
 
 status.__doc__ = _('Print count of the recent crashes')

src/cli/abrtcli/l18n.py
@@ -5,7 +5,7 @@ import logging
 GETTEXT_PROGNAME = "abrt"
 
 _ = gettext.gettext
-ngettext = gettext.ngettext
+N_ = gettext.ngettext
 
 from .config import LOCALE_DIR
 
@@ -14,13 +14,10 @@ def init():
     progname = GETTEXT_PROGNAME
     localedir = LOCALE_DIR
 
-    lcl = 'C'
     try:
-        lcl = locale.setlocale(locale.LC_ALL, "")
+        locale.setlocale(locale.LC_ALL, "")
     except locale.Error:
-        import os
-        os.environ['LC_ALL'] = 'C'
-        lcl = locale.setlocale(locale.LC_ALL, "")
+        locale.setlocale(locale.LC_ALL, "C")
 
     gettext.bindtextdomain(progname, localedir)
     gettext.textdomain(progname)

src/cli/abrtcli/match.py
@@ -94,14 +94,14 @@ def match_get_problem(problem_match, allow_multiple=False, authenticate=False):
     if problem_match == 'last':
         probs = sort_problems(problem.list(auth=authenticate))
         if not probs:
-            print(_('No problems'))
+            print(_('No problems found'))
             sys.exit(0)
 
         prob = probs[0]
     else:
         probs = match_lookup(problem_match, authenticate=authenticate)
         if not probs:
-            print(_('No problem(s) matched'))
+            print(_('No matching problems found'))
             sys.exit(1)
         elif len(probs) > 1:
             if allow_multiple:

src/cli/tests/test_match.py
@@ -150,7 +150,7 @@ class MatchTestCase(clitests.TestCase):
                     match_get_problem('nope')
 
             stdout = cap_stdout.getvalue()
-            self.assertIn("No problem(s) matched", stdout)
+            self.assertIn("No matching problems found", stdout)
 
             # Similar with last
             with captured_output() as (cap_stdout, cap_stderr):

commit cd35cf6244250a2d53fc87e450c10b4ebc5f473c

cli: Fix warning
590a7a89a1e23998369fb54fa4fcaa519e0e4553 inadvertently passes too many
arguments to format(), leading to string using the problem count and not
timestamp.

Signed-off-by: Ernestas Kulik <ekulik@redhat.com>

src/cli/abrtcli/cli.py
@@ -349,9 +349,9 @@ def status(args):
 
     if not args.quiet or len(probs) > 0:
         print(N_('ABRT has detected a problem. For more information, run “abrt list{}”',
-                 'ABRT has detected {} problems. For more information, run “abrt list{}”',
+                 'ABRT has detected %d problems. For more information, run “abrt list{}”' % (len(probs)),
                  len(probs))
-              .format(len(probs), since_append))
+              .format(since_append))
 
 status.__doc__ = _('Print count of the recent crashes')
 