abrt/abrt

commit 5284fd63f1d2d9da09fd1793e49ab734557ea046

koops: Avoid explicit dependency on hash size
Change the API so that koops_hash_str and koops_hash_str_ext return the
heap-allocated encoded digest instead of taking a pointer and returning
a status code.

This eliminates the need to know the size of the resulting hash in
advance, which should solve the build problems following recent
libreport changes.

src/plugins/abrt-action-analyze-oops.c
@@ -60,9 +60,8 @@ int main(int argc, char **argv)
     load_abrt_plugin_conf_file("oops.conf", settings);
 
     char *oops = dd_load_text(dd, FILENAME_BACKTRACE);
-    char hash_str[SHA1_RESULT_LEN*2 + 1];
-    int bad = koops_hash_str(hash_str, oops);
-    if (bad)
+    g_autofree char *hash_str = koops_hash_str(oops);
+    if (!hash_str)
     {
         error_msg("Can't find a meaningful backtrace for hashing in '%s'", dump_dir_name);
 
@@ -87,7 +86,7 @@ int main(int argc, char **argv)
             /* We need UUID file for the local duplicates look-up and DUPHASH */
             /* file is also useful because user can force ABRT to report */
             /* the oops into a bug tracking system (Bugzilla). */
-            bad = koops_hash_str_ext(hash_str, oops,
+            hash_str = koops_hash_str_ext(oops,
                     /* use no frame count limit */-1,
                     /* use every frame in stacktrace */0);
 
@@ -97,7 +96,7 @@ int main(int argc, char **argv)
 
     free(oops);
 
-    if (!bad)
+    if (hash_str)
     {
         dd_save_text(dd, FILENAME_UUID, hash_str);
         dd_save_text(dd, FILENAME_DUPHASH, hash_str);
@@ -107,5 +106,5 @@ int main(int argc, char **argv)
 
     free_map_string(settings);
 
-    return bad;
+    return NULL != hash_str;
 }

src/include/libabrt.h
@@ -138,9 +138,9 @@ char *kernel_tainted_short(const char *kernel_bt);
 #define kernel_tainted_long abrt_kernel_tainted_long
 char *kernel_tainted_long(const char *tainted_short);
 #define koops_hash_str_ext abrt_koops_hash_str_ext
-int koops_hash_str_ext(char hash_str[SHA1_RESULT_LEN*2 + 1], const char *oops_buf, int frame_count, int duphas_flags);
+char *koops_hash_str_ext(const char *oops_buf, int frame_count, int duphas_flags);
 #define koops_hash_str abrt_koops_hash_str
-int koops_hash_str(char hash_str[SHA1_RESULT_LEN*2 + 1], const char *oops_buf);
+char *koops_hash_str(const char *oops_buf);
 
 
 #define koops_line_skip_level abrt_koops_line_skip_level

src/lib/kernel.c
@@ -586,18 +586,16 @@ void koops_extract_oopses_from_lines(GList **oops_list, const struct abrt_koops_
     }
 }
 
-int koops_hash_str_ext(char result[SHA1_RESULT_LEN*2 + 1], const char *oops_buf, int frame_count, int duphash_flags)
+char *koops_hash_str_ext(const char *oops_buf, int frame_count, int duphash_flags)
 {
-    char *hash_str = NULL, *error = NULL;
-    int bad = 0;
+    g_autofree char *error = NULL;
+    char *digest = NULL;
 
     struct sr_stacktrace *stacktrace = sr_stacktrace_parse(SR_REPORT_KERNELOOPS,
                                                            oops_buf, &error);
     if (!stacktrace)
     {
         log_debug("Failed to parse koops: %s", error);
-        free(error);
-        bad = 1;
         goto end;
     }
 
@@ -605,43 +603,34 @@ int koops_hash_str_ext(char result[SHA1_RESULT_LEN*2 + 1], const char *oops_buf,
     if (!thread)
     {
         log_debug("Failed to find crash thread");
-        bad = 1;
         goto end;
     }
 
     if (g_verbose >= 3)
     {
-        hash_str = sr_thread_get_duphash(thread, frame_count, NULL,
-                                         duphash_flags|SR_DUPHASH_NOHASH);
-        if (hash_str)
-            log_warning("Generating duphash: '%s'", hash_str);
+        digest = sr_thread_get_duphash(thread, frame_count, NULL,
+                                       duphash_flags|SR_DUPHASH_NOHASH);
+        if (digest)
+        {
+            log_warning("Generating duphash: '%s'", digest);
+            free(digest);
+        }
         else
             log_warning("Nothing useful for duphash");
-
-
-        free(hash_str);
     }
 
-    hash_str = sr_thread_get_duphash(thread, frame_count, NULL, duphash_flags);
-    if (hash_str)
-    {
-        strncpy(result, hash_str, SHA1_RESULT_LEN*2);
-        result[SHA1_RESULT_LEN*2] = '\0';
-        free(hash_str);
-    }
-    else
-        bad = 1;
+    digest = sr_thread_get_duphash(thread, frame_count, NULL, duphash_flags);
 
 end:
     sr_stacktrace_free(stacktrace);
-    return bad;
+    return digest;
 }
 
-int koops_hash_str(char result[SHA1_RESULT_LEN*2 + 1], const char *oops_buf)
+char *koops_hash_str(const char *oops_buf)
 {
     const int frame_count = 6;
     const int duphash_flags = SR_DUPHASH_NONORMALIZE|SR_DUPHASH_KOOPS_COMPAT;
-    return koops_hash_str_ext(result, oops_buf, frame_count, duphash_flags);
+    return koops_hash_str_ext(oops_buf, frame_count, duphash_flags);
 }
 
 char *koops_extract_version(const char *linepointer)

tests/koops-parser.at
@@ -125,16 +125,14 @@ AT_TESTFUN([koops_hash_improve],
 
 int run_test(const struct test_struct *test)
 {
-	char *oops1 = fread_full(test->filename);
-	char *oops2 = fread_full(test->expected_results);
+	g_autofree char *oops1 = fread_full(test->filename);
+	g_autofree char *oops2 = fread_full(test->expected_results);
 
-	char hash_oops1[SHA1_RESULT_LEN*2 + 1];
-	koops_hash_str(hash_oops1, oops1);
-	free(oops1);
+	g_autofree char *hash_oops1 = koops_hash_str(oops1);
+	g_autofree char *hash_oops2 = koops_hash_str(oops2);
 
-	char hash_oops2[SHA1_RESULT_LEN*2 + 1];
-	koops_hash_str(hash_oops2, oops2);
-	free(oops2);
+	if (NULL == hash_oops1 || NULL == hash_oops2)
+	    return 1;
 
 	if (!strcmp(hash_oops1, hash_oops2))
 		return 0;

commit d49c8d68ccb29b848ed86b61b17030f2b49ca696

plugins: abrt-action-analyze-oops: Refactor code
This commit GLib-ifies the code and fixes the return code in case of
success, as currently NULL != hash_str is returned, which will be the
opposite of what it should be
(caused by 5284fd63f1d2d9da09fd1793e49ab734557ea046, my bad).

This is one step towards getting rid of the libreport command-line
parsing nonsense.

src/plugins/abrt-action-analyze-oops.c
@@ -21,7 +21,34 @@
 
 int main(int argc, char **argv)
 {
-    /* I18n */
+    const char *dump_directory = ".";
+    const GOptionEntry option_entries[] =
+    {
+        {
+            "verbose",
+            'v',
+            G_OPTION_FLAG_NONE,
+            G_OPTION_ARG_NONE, &g_verbose,
+            "Be verbose",
+            NULL,
+        },
+        {
+            "directory",
+            'd',
+            G_OPTION_FLAG_NONE,
+            G_OPTION_ARG_FILENAME, &dump_directory,
+            "Problem directory",
+            "DIR",
+        },
+        { NULL, },
+    };
+    g_autoptr(GOptionContext) option_context = NULL;
+    g_autoptr(GError) error = NULL;
+    struct dump_dir *dd;
+    g_autoptr(GHashTable) settings = NULL;
+    g_autofree char *oops = NULL;
+    g_autofree char *hash_str = NULL;
+
     setlocale(LC_ALL, "");
 #if ENABLE_NLS
     bindtextdomain(PACKAGE, LOCALEDIR);
@@ -30,40 +57,34 @@ int main(int argc, char **argv)
 
     abrt_init(argv);
 
-    const char *dump_dir_name = ".";
+    option_context = g_option_context_new (NULL);
 
-    /* Can't keep these strings/structs static: _() doesn't support that */
-    const char *program_usage_string = _(
-        "& [-v] -d DIR\n"
-        "\n"
-        "Calculates and saves UUID and DUPHASH for oops problem directory DIR"
-        );
-    enum {
-        OPT_v = 1 << 0,
-        OPT_d = 1 << 1,
-    };
-    /* Keep enum above and order of options below in sync! */
-    struct options program_options[] = {
-        OPT__VERBOSE(&g_verbose),
-        OPT_STRING('d', NULL, &dump_dir_name, "DIR", _("Problem directory")),
-        OPT_END()
-    };
-    /*unsigned opts =*/ parse_opts(argc, argv, program_options, program_usage_string);
+    g_option_context_add_main_entries(option_context, option_entries, PACKAGE);
+    g_option_context_set_summary(option_context,
+                                 _("Calculates and saves UUID and DUPHASH for oops problem directory DIR"));
+    if (!g_option_context_parse(option_context, &argc, &argv, &error))
+    {
+        error_msg("Parsing command-line options failed: %s", error->message);
+
+        return EXIT_FAILURE;
+    }
 
     export_abrt_envvars(0);
 
-    struct dump_dir *dd = dd_opendir(dump_dir_name, /*flags:*/ 0);
-    if (!dd)
-        return 1;
+    dd = dd_opendir(dump_directory, /*flags:*/ 0);
+    if (NULL == dd)
+    {
+        return EXIT_FAILURE;
+    }
+    settings = g_hash_table_new_full(g_str_hash, g_str_equal, g_free, g_free);
 
-    map_string_t *settings = new_map_string();
     load_abrt_plugin_conf_file("oops.conf", settings);
 
-    char *oops = dd_load_text(dd, FILENAME_BACKTRACE);
-    g_autofree char *hash_str = koops_hash_str(oops);
-    if (!hash_str)
+    oops = dd_load_text(dd, FILENAME_BACKTRACE);
+    hash_str = koops_hash_str(oops);
+    if (NULL == hash_str)
     {
-        error_msg("Can't find a meaningful backtrace for hashing in '%s'", dump_dir_name);
+        error_msg("Can't find a meaningful backtrace for hashing in '%s'", dump_directory);
 
         /* Do not drop such oopses by default. */
         int drop_notreportable_oopses = 0;
@@ -72,7 +93,7 @@ int main(int argc, char **argv)
         if (!res || !drop_notreportable_oopses)
         {
             /* Let users know that they can configure ABRT to drop these oopses. */
-            log_warning("Preserving oops '%s' because DropNotReportableOopses is 'no'", dump_dir_name);
+            log_warning("Preserving oops '%s' because DropNotReportableOopses is 'no'", dump_directory);
 
             dd_save_text(dd, FILENAME_NOT_REPORTABLE,
             _("The backtrace does not contain enough meaningful function frames "
@@ -93,10 +114,7 @@ int main(int argc, char **argv)
             /* If even this attempt fails, we can drop the oops without any hesitation. */
         }
     }
-
-    free(oops);
-
-    if (hash_str)
+    if (NULL != hash_str)
     {
         dd_save_text(dd, FILENAME_UUID, hash_str);
         dd_save_text(dd, FILENAME_DUPHASH, hash_str);
@@ -104,7 +122,10 @@ int main(int argc, char **argv)
 
     dd_close(dd);
 
-    free_map_string(settings);
+    if (NULL == hash_str)
+    {
+        return EXIT_FAILURE;
+    }
 
-    return NULL != hash_str;
+    return EXIT_SUCCESS;
 }