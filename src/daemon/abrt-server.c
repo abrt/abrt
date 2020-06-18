abrt/abrt

commit 89e0ddda37d8eb9b3aff78858be87255b36379dc

Use g_autofree where possible
Signed-off-by: Michal Fabik <mfabik@redhat.com>

src/daemon/abrt-server.c
@@ -228,7 +228,7 @@ static pid_t spawn_event_handler_child(const char *dump_dir_name, const char *ev
 
 static int problem_dump_dir_was_provoked_by_abrt_event(struct dump_dir *dd, char  **provoker)
 {
-    char *env_var = NULL;
+    g_autofree char *env_var = NULL;
     const int r = dd_get_env_variable(dd, ABRT_SERVER_EVENT_ENV, &env_var);
 
     /* Dump directory doesn't contain the environ file */
@@ -237,8 +237,6 @@ static int problem_dump_dir_was_provoked_by_abrt_event(struct dump_dir *dd, char
 
     if (provoker != NULL)
         *provoker = env_var;
-    else
-        free(env_var);
 
     return env_var != NULL;
 }
@@ -294,7 +292,7 @@ static int run_post_create(const char *dirname, struct response *resp)
     {
         struct dump_dir *dd = dd_opendir(dirname, DD_OPEN_READONLY);
 
-        char *provoker = NULL;
+        g_autofree char *provoker = NULL;
         const bool event_dir = dd && problem_dump_dir_was_provoked_by_abrt_event(dd, &provoker);
         if (event_dir)
         {
@@ -321,7 +319,6 @@ static int run_post_create(const char *dirname, struct response *resp)
 
             }
 
-            free(provoker);
             return 400;
         }
 
@@ -381,7 +378,7 @@ static int run_post_create(const char *dirname, struct response *resp)
     int child_stdout_fd;
     int child_pid = spawn_event_handler_child(dirname, "post-create", &child_stdout_fd);
 
-    char *dup_of_dir = NULL;
+    g_autofree char *dup_of_dir = NULL;
     GString *cmd_output = g_string_new(NULL);
 
     bool child_is_post_create = 1; /* else it is a notify child */
@@ -411,7 +408,6 @@ static int run_post_create(const char *dirname, struct response *resp)
             if (child_is_post_create
              && g_str_has_prefix(msg, "DUP_OF_DIR: "))
             {
-                free(dup_of_dir);
                 dup_of_dir = g_strdup(msg + strlen("DUP_OF_DIR: "));
             }
             else
@@ -488,7 +484,7 @@ static int run_post_create(const char *dirname, struct response *resp)
         {
             /* Update the last occurrence file by the time file of the new problem */
             struct dump_dir *new_dd = dd_opendir(dirname, DD_OPEN_READONLY);
-            char *last_ocr = NULL;
+            g_autofree char *last_ocr = NULL;
             if (new_dd)
             {
                 /* TIME must exists in a valid dump directory but we don't want to die
@@ -510,8 +506,6 @@ static int run_post_create(const char *dirname, struct response *resp)
             }
 
             dd_save_text(dd, FILENAME_LAST_OCCURRENCE, last_ocr);
-
-            free(last_ocr);
         }
     }
 
@@ -545,7 +539,6 @@ static int run_post_create(const char *dirname, struct response *resp)
     else
     {
         RESPONSE_SETTER(resp, 200, NULL);
-        free(dup_of_dir);
     }
     dup_of_dir = NULL;
     g_string_erase(cmd_output, 0, -1);
@@ -559,7 +552,6 @@ static int run_post_create(const char *dirname, struct response *resp)
 
  ret:
     g_string_free(cmd_output, TRUE);
-    free(dup_of_dir);
     close(child_stdout_fd);
     return 0;
 }
@@ -605,7 +597,7 @@ static int create_problem_dir(GHashTable *problem_info, unsigned pid)
     }
 
     const int proc_dir_fd = libreport_open_proc_pid_dir(pid);
-    char *rootdir = NULL;
+    g_autofree char *rootdir = NULL;
 
     if (proc_dir_fd < 0)
     {
@@ -638,19 +630,17 @@ static int create_problem_dir(GHashTable *problem_info, unsigned pid)
     if (proc_dir_fd >= 0)
     {
         /* Obtain and save the command line. */
-        char *cmdline = libreport_get_cmdline_at(proc_dir_fd);
+        g_autofree char *cmdline = libreport_get_cmdline_at(proc_dir_fd);
         if (cmdline)
         {
             dd_save_text(dd, FILENAME_CMDLINE, cmdline);
-            free(cmdline);
         }
 
         /* Obtain and save the environment variables. */
-        char *environ = libreport_get_environ_at(proc_dir_fd);
+        g_autofree char *environ = libreport_get_environ_at(proc_dir_fd);
         if (environ)
         {
             dd_save_text(dd, FILENAME_ENVIRON, environ);
-            free(environ);
         }
 
         dd_copy_file_at(dd, FILENAME_CGROUP,    proc_dir_fd, "cgroup");
@@ -691,9 +681,8 @@ static int create_problem_dir(GHashTable *problem_info, unsigned pid)
                 pid_t container_pid;
                 if (libreport_get_pid_of_container_at(proc_dir_fd, &container_pid) == 0)
                 {
-                    char *container_cmdline = libreport_get_cmdline(container_pid);
+                    g_autofree char *container_cmdline = libreport_get_cmdline(container_pid);
                     dd_save_text(dd, FILENAME_CONTAINER_CMDLINE, container_cmdline);
-                    free(container_cmdline);
                 }
             }
             else
@@ -703,7 +692,6 @@ static int create_problem_dir(GHashTable *problem_info, unsigned pid)
         }
         close(proc_dir_fd);
     }
-    free(rootdir);
 
     /* Store id of the user whose application crashed. */
     char uid_str[sizeof(long) * 3 + 2];
@@ -729,10 +717,9 @@ static int create_problem_dir(GHashTable *problem_info, unsigned pid)
     /* Move the completely created problem directory
      * to final directory.
      */
-    char *newpath = g_strndup(path, strlen(path) - strlen(".new"));
+    g_autofree char *newpath = g_strndup(path, strlen(path) - strlen(".new"));
     if (rename(path, newpath) == 0)
         strcpy(path, newpath);
-    free(newpath);
 
     log_notice("Saved problem directory of pid %u to '%s'", pid, path);
 
@@ -793,7 +780,8 @@ static gboolean key_value_ok(gchar *key, gchar *value)
 /* Handles a message received from client over socket. */
 static void process_message(GHashTable *problem_info, char *message)
 {
-    gchar *key, *value;
+    g_autofree gchar *key = NULL;
+    gchar *value;
 
     value = strchr(message, '=');
     if (value)
@@ -821,7 +809,6 @@ static void process_message(GHashTable *problem_info, char *message)
             /* should use error_msg_and_die() here? */
             error_msg("Invalid key or value format: %s", message);
         }
-        free(key);
     }
     else
     {
@@ -1048,9 +1035,8 @@ static int perform_http_xact(struct response *rsp)
     char *executable = g_hash_table_lookup(problem_info, FILENAME_EXECUTABLE);
     if (executable)
     {
-        char *last_file = g_build_filename(abrt_g_settings_dump_location ? abrt_g_settings_dump_location : "", "last-via-server", NULL);
+        g_autofree char *last_file = g_build_filename(abrt_g_settings_dump_location ? abrt_g_settings_dump_location : "", "last-via-server", NULL);
         int repeating_crash = check_recent_crash_file(last_file, executable);
-        free(last_file);
         if (repeating_crash) /* Only pretend that we saved it */
         {
             error_msg("Not saving repeating crash in '%s'", executable);

src/applet/test_report.c
@@ -43,7 +43,7 @@ int main(int argc, char** argv)
     problem_data_free(problem_data);
     if (!dd)
         return 1;
-    char *dir_name = strdup(dd->dd_dirname);
+    g_autofree char *dir_name = strdup(dd->dd_dirname);
     dd_close(dd);
 
     printf("Temp dump dir: '%s'\n", dir_name);
@@ -56,7 +56,6 @@ int main(int argc, char** argv)
     free_run_event_state(run_state);
 
 //    delete_dump_dir(dir_name);
-    free(dir_name);
 
     return 0;
 }

src/configuration-gui/abrt-config-widget.c
@@ -463,15 +463,15 @@ abrt_config_widget_init(AbrtConfigWidget *self)
             gpp_app = "gnome-privacy-panel.desktop";
 
         GDesktopAppInfo *app = g_desktop_app_info_new(gpp_app);
-        char *message = NULL;
-        char *markup = NULL;
+        g_autofree char *message = NULL;
+        g_autofree char *markup = NULL;
         if (!app)
         {
             /* Make the switch editable */
             self->priv->options[ABRT_OPT_SEND_UREPORT].config = self->priv->privacy_gsettings;
 
-            char *os_release = libreport_xmalloc_open_read_close("/etc/os-release", /*no size limit*/NULL);
-            char *privacy_policy = NULL;
+            g_autofree char *os_release = libreport_xmalloc_open_read_close("/etc/os-release", /*no size limit*/NULL);
+            g_autofree char *privacy_policy = NULL;
 
             /* Try to get the value of PRIVACY_POLICY from /etc/os-release */
             sr_parse_os_release(os_release, os_release_callback, (void *)&privacy_policy);
@@ -486,8 +486,6 @@ abrt_config_widget_init(AbrtConfigWidget *self)
             else
                 markup = g_strdup_printf("<i>%s</i>", message);
 
-            free(privacy_policy);
-            free(os_release);
         }
         else
         {
@@ -529,9 +527,6 @@ abrt_config_widget_init(AbrtConfigWidget *self)
         gtk_label_set_line_wrap(GTK_LABEL(lbl), TRUE);
         /* Let users to copy the warning. */
         gtk_label_set_selectable(GTK_LABEL(lbl), TRUE);
-
-        free(markup);
-        free(message);
 
         gtk_box_pack_start(GTK_BOX(hbox_auto_reporting), lbl, false, false, 0);
     }

src/daemon/abrt-action-save-container-data.c
@@ -25,9 +25,8 @@ void dump_docker_info(struct dump_dir *dd, const char *root_dir)
         dd_save_text(dd, FILENAME_CONTAINER, "docker");
 
     json_object *json = NULL;
-    char *mntnf_path = g_build_filename(dd->dd_dirname ? dd->dd_dirname : "", FILENAME_MOUNTINFO, NULL);
+    g_autofree char *mntnf_path = g_build_filename(dd->dd_dirname ? dd->dd_dirname : "", FILENAME_MOUNTINFO, NULL);
     FILE *mntnf_file = fopen(mntnf_path, "r");
-    free(mntnf_path);
 
     struct mount_point {
         const char *name;
@@ -41,7 +40,7 @@ void dump_docker_info(struct dump_dir *dd, const char *root_dir)
     };
 
     char *container_id = NULL;
-    char *output = NULL;
+    g_autofree char *output = NULL;
 
     /* initialized to 0 because we call libreport_mountinfo_destroy below */
     struct mountinfo mntnf = {0};
@@ -101,7 +100,7 @@ void dump_docker_info(struct dump_dir *dd, const char *root_dir)
             continue;
         }
 
-        char *docker_inspect_cmdline = NULL;
+        g_autofree char *docker_inspect_cmdline = NULL;
         if (root_dir != NULL)
             docker_inspect_cmdline = g_strdup_printf("chroot %s /bin/sh -c \"docker inspect %s\"", root_dir, container_id);
         else
@@ -110,8 +109,6 @@ void dump_docker_info(struct dump_dir *dd, const char *root_dir)
         log_debug("Executing: '%s'", docker_inspect_cmdline);
         output = libreport_run_in_shell_and_save_output(0, docker_inspect_cmdline, "/", NULL);
 
-        free(docker_inspect_cmdline);
-
         if (output == NULL || strcmp(output, "[]\n") == 0)
         {
             log_debug("Unsupported container ID: '%s'", container_id);
@@ -119,7 +116,6 @@ void dump_docker_info(struct dump_dir *dd, const char *root_dir)
             free(container_id);
             container_id = NULL;
 
-            free(output);
             output = NULL;
 
             continue;
@@ -139,7 +135,6 @@ void dump_docker_info(struct dump_dir *dd, const char *root_dir)
     dd_save_text(dd, FILENAME_DOCKER_INSPECT, output);
 
     json = json_tokener_parse(output);
-    free(output);
 
     if (json == NULL)
     {
@@ -186,9 +181,8 @@ void dump_lxc_info(struct dump_dir *dd, const char *lxc_cmd)
     if (!dd_exist(dd, FILENAME_CONTAINER))
         dd_save_text(dd, FILENAME_CONTAINER, "lxc");
 
-    char *mntnf_path = g_build_filename(dd->dd_dirname ? dd->dd_dirname : "", FILENAME_MOUNTINFO, NULL);
+    g_autofree char *mntnf_path = g_build_filename(dd->dd_dirname ? dd->dd_dirname : "", FILENAME_MOUNTINFO, NULL);
     FILE *mntnf_file = fopen(mntnf_path, "r");
-    free(mntnf_path);
 
     struct mountinfo mntnf;
     int r = libreport_get_mountinfo_for_mount_point(mntnf_file, &mntnf, "/");
@@ -274,7 +268,7 @@ int main(int argc, char **argv)
     if (dd == NULL)
         libreport_xfunc_die();
 
-    char *container_cmdline = dd_load_text_ext(dd, FILENAME_CONTAINER_CMDLINE, DD_LOAD_TEXT_RETURN_NULL_ON_FAILURE);
+    g_autofree char *container_cmdline = dd_load_text_ext(dd, FILENAME_CONTAINER_CMDLINE, DD_LOAD_TEXT_RETURN_NULL_ON_FAILURE);
     if (container_cmdline == NULL)
         error_msg_and_die("The crash didn't occur in container");
 
@@ -285,7 +279,6 @@ int main(int argc, char **argv)
     else
         error_msg_and_die("Unsupported container technology");
 
-    free(container_cmdline);
     dd_close(dd);
 
     return 0;

src/daemon/abrt-action-save-package-data.c
@@ -263,17 +263,16 @@ static int SavePackageDescriptionToDebugDump(const char *dump_dir_name, const ch
     if (!dd)
         return 1;
 
-    char *type = dd_load_text(dd, FILENAME_TYPE);
+    g_autofree char *type = dd_load_text(dd, FILENAME_TYPE);
     bool kernel_oops = !strcmp(type, "Kerneloops") || !strcmp(type, "vmcore");
-    free(type);
 
-    char *cmdline = NULL;
-    char *executable = NULL;
-    char *rootdir = NULL;
-    char *package_short_name = NULL;
-    char *fingerprint = NULL;
+    g_autofree char *cmdline = NULL;
+    g_autofree char *executable = NULL;
+    g_autofree char *rootdir = NULL;
+    g_autofree char *package_short_name = NULL;
+    g_autofree char *fingerprint = NULL;
     struct pkg_nevra *pkg_name = NULL;
-    char *component = NULL;
+    g_autofree char *component = NULL;
     char *kernel = NULL;
     int error = 1;
     /* note: "goto ret" statements below free all the above variables,
@@ -457,13 +456,7 @@ skip_interpreter:
     if (dd)
         dd_close(dd);
 
-    free(cmdline);
-    free(executable);
-    free(rootdir);
-    free(package_short_name);
     free_pkg_nevra(pkg_name);
-    free(component);
-    free(fingerprint);
 
     return error;
 }

src/daemon/abrt-auto-reporting.c
@@ -330,7 +330,7 @@ int main(int argc, char *argv[])
 
     if ((opts & OPT_u))
     {
-        char *tmp_password = NULL;
+        g_autofree char *tmp_password = NULL;
         if (!(opts & OPT_p))
         {
             password = tmp_password = libreport_ask_password(_("Password:"));
@@ -353,8 +353,6 @@ int main(int argc, char *argv[])
             libreport_save_plugin_conf_file(UREPORT_NAME, ureport_conf_bck);
             goto finito;
         }
-
-        free(tmp_password);
     }
 
     if ((opts & OPT_c))

src/daemon/abrt-handle-event.c
@@ -229,7 +229,7 @@ static int is_crash_a_dup(const char *dump_dir_name, void *param)
     type = dd_load_text(dd, FILENAME_TYPE);
     free(executable);
     executable = dd_load_text_ext(dd, FILENAME_EXECUTABLE, DD_FAIL_QUIETLY_ENOENT);
-    char *container_id = dd_load_text_ext(dd, FILENAME_CONTAINER_ID, DD_FAIL_QUIETLY_ENOENT);
+    g_autofree char *container_id = dd_load_text_ext(dd, FILENAME_CONTAINER_ID, DD_FAIL_QUIETLY_ENOENT);
     dup_uuid_init(dd);
     dup_corebt_init(dd);
     dd_close(dd);
@@ -256,7 +256,7 @@ static int is_crash_a_dup(const char *dump_dir_name, void *param)
 
         char *tmp_concat_path = g_build_filename(abrt_g_settings_dump_location, dent->d_name, NULL);
 
-        char *dump_dir_name2 = realpath(tmp_concat_path, NULL);
+        g_autofree char *dump_dir_name2 = realpath(tmp_concat_path, NULL);
         if (libreport_g_verbose > 1 && !dump_dir_name2)
             perror_msg("realpath(%s)", tmp_concat_path);
 
@@ -265,8 +265,8 @@ static int is_crash_a_dup(const char *dump_dir_name, void *param)
         if (!dump_dir_name2)
             continue;
 
-        char *dd_uid = NULL, *dd_type = NULL;
-        char *dd_executable = NULL, *dd_container_id = NULL;
+        g_autofree char *dd_uid = NULL, *dd_type = NULL;
+        g_autofree char *dd_executable = NULL, *dd_container_id = NULL;
 
         if (strcmp(dump_dir_name, dump_dir_name2) == 0)
             goto next; /* we are never a dup of ourself */
@@ -323,17 +323,12 @@ static int is_crash_a_dup(const char *dump_dir_name, void *param)
         }
 
 next:
-        free(dump_dir_name2);
         dd_close(dd);
-        free(dd_uid);
-        free(dd_type);
-        free(dd_container_id);
     }
     closedir(dir);
 
 end:
     free((char*)dump_dir_name);
-    free(container_id);
     return retval;
 }
 
@@ -402,7 +397,7 @@ int main(int argc, char **argv)
     }
 
     bool post_create = (strcmp(event_name, "post-create") == 0);
-    char *dump_dir_name = NULL;
+    g_autofree char *dump_dir_name = NULL;
     while (*argv)
     {
         dump_dir_name = g_strdup(*argv++);
@@ -458,7 +453,6 @@ int main(int argc, char **argv)
         if (r != 0)
             return r; /* yes */
 
-        free(dump_dir_name);
         dump_dir_name = NULL;
     }
 

src/daemon/abrt-inotify.c
@@ -62,7 +62,7 @@ static gboolean handle_inotify_cb(GIOChannel *gio, GIOCondition condition, gpoin
      * and we were going out of sync wrt struct inotify_event's layout.
      */
     inotify_bytes += 2 * (sizeof(struct inotify_event) + FILENAME_MAX);
-    char *buf = g_malloc(inotify_bytes);
+    g_autofree char *buf = g_malloc(inotify_bytes);
     errno = 0;
     gsize len;
     GError *gerror = NULL;
@@ -73,7 +73,6 @@ static gboolean handle_inotify_cb(GIOChannel *gio, GIOCondition condition, gpoin
     if (err != G_IO_STATUS_NORMAL)
     {
         error_msg("Error reading inotify fd: %s", gerror ? gerror->message : "unknown");
-        free(buf);
         if (gerror)
             g_error_free(gerror);
         return FALSE; /* "remove this event" (huh??) */
@@ -96,7 +95,6 @@ static gboolean handle_inotify_cb(GIOChannel *gio, GIOCondition condition, gpoin
 
         aic->handler(aic, event, aic->user_data);
     }
-    free(buf);
     return TRUE;
 }
 

commit a53b6e953f798c6de3000a28060cd3bdff6844ca

abrt-server: Fix double free and possible leaks
Fallout from 89e0ddda. g_autofree'd dup_of_dir would get free'd in the
RESPONSE_SETTER macro and it was getting reassigned with g_strdup inside
a loop anyway.

Signed-off-by: Michal Fabik <mfabik@redhat.com>

src/daemon/abrt-server.c
@@ -378,7 +378,7 @@ static int run_post_create(const char *dirname, struct response *resp)
     int child_stdout_fd;
     int child_pid = spawn_event_handler_child(dirname, "post-create", &child_stdout_fd);
 
-    g_autofree char *dup_of_dir = NULL;
+    char *dup_of_dir = NULL;
     g_autoptr(GString) cmd_output = g_string_new(NULL);
 
     bool child_is_post_create = 1; /* else it is a notify child */
@@ -408,6 +408,7 @@ static int run_post_create(const char *dirname, struct response *resp)
             if (child_is_post_create
              && g_str_has_prefix(msg, "DUP_OF_DIR: "))
             {
+                free(dup_of_dir);
                 dup_of_dir = g_strdup(msg + strlen("DUP_OF_DIR: "));
             }
             else
@@ -539,6 +540,7 @@ static int run_post_create(const char *dirname, struct response *resp)
     else
     {
         RESPONSE_SETTER(resp, 200, NULL);
+        free(dup_of_dir);
     }
     dup_of_dir = NULL;
     g_string_erase(cmd_output, 0, -1);
@@ -551,6 +553,7 @@ static int run_post_create(const char *dirname, struct response *resp)
     RESPONSE_SETTER(resp, 403, NULL);
 
  ret:
+    free(dup_of_dir);
     close(child_stdout_fd);
     return 0;
 }